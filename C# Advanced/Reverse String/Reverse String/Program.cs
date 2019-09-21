df4077bad55905175db024755ed8955ddc17154c
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-09-21 23:54:30 +0300

    Task 1

---
+++ C# Advanced/Reverse String/Reverse String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var stack = new Stack<char>();
+
+            foreach (var @char in input)
+            {
+                stack.Push(@char);
+            }
+
+            foreach (var @char in stack)
+            {
+                Console.Write(@char);
+            }
+        }
+    }
+}
