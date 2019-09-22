20ccdcee7980094651aa1ad6b27be2b3578fb6d6
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-09-23 00:46:42 +0300

    Print Even Numbers Task

---
+++ C# Advanced/Print Even Numbers/Print Even Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse);
+            var queue = new Queue<int>();
+            string result = string.Empty;
+            foreach (var num in numbers)
+            {
+                queue.Enqueue(num);
+            }
+            int turns = queue.Count();
+            for (int i = 0; i < turns; i++)
+            {
+                int @integer = queue.Peek();
+                if (@integer % 2 != 0)
+                {
+                    queue.Dequeue();
+                }
+                else
+                {
+                    queue.Dequeue();
+                    queue.Enqueue(@integer);
+                }
+            }
+            Console.WriteLine(string.Join(", ", queue));
+        }
+    }
+}
