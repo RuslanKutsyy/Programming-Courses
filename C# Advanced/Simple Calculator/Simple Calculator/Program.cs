4d2af5598d01652bba0a65e4c12b198dbbd78745
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-09-22 23:48:27 +0300

    New Exercise from the Course

---
+++ C# Advanced/Simple Calculator/Simple Calculator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            var stack = new Stack<string>();
+            int sum = 0;
+            int count = 1;
+
+            foreach (var item in input.Reverse())
+            {
+                stack.Push(item);
+            }
+
+            while (true)
+            {
+                if (stack.Count==0)
+                {
+                    break;
+                }
+
+                int num1 = sum;
+                string @operator = string.Empty;
+
+                if (count == 1)
+                {
+                    @operator = "+";
+                    count = 0;
+                }
+                else
+                {
+                    @operator = stack.Pop();
+                }
+
+                int num2 = int.Parse(stack.Pop());
+
+                switch (@operator)
+                {
+                    case "+":
+                        {
+                            sum = num1 + num2;
+                            break;
+                        }
+                    case "-":
+                        {
+                            sum = num1 - num2;
+                            break;
+                        }
+
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
