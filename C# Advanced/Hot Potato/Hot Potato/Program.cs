60d95886d554c1766caf5e812544080352834a92
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-09-23 01:35:41 +0300

    Hot Potato Exercise

---
+++ C# Advanced/Hot Potato/Hot Potato/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine().Split();
+            int count = int.Parse(Console.ReadLine());
+            var queue = new Queue<string>(names);
+
+            while (queue.Count>1)
+            {
+                for (int i = 1; i <= count; i++)
+                {
+                    if (i!=count)
+                    {
+                        string name = queue.Dequeue();
+                        queue.Enqueue(name);
+                    }
+                    else
+                    {
+                        string name = queue.Dequeue();
+                        Console.WriteLine($"Removed {name}");
+                    }
+                }
+            }
+            Console.WriteLine($"Last is {queue.Peek()}");
+        }
+    }
+}
