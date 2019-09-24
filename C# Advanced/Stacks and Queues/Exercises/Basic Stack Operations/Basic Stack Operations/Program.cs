7475c861b62b8b575a3488cf23461e6f64d5facd
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-09-24 22:51:45 +0300

    This task is not fully ready

--- C# Advanced/Stacks and Queues/Exercises/Basic Stack Operations/Basic Stack Operations/Program.cs
+++ C# Advanced/Stacks and Queues/Exercises/Basic Stack Operations/Basic Stack Operations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Basic_Stack_Operations
 {
@@ -6,7 +8,45 @@ namespace Basic_Stack_Operations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] data = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>();
+            int minValue = int.MaxValue;
+            int numsToPush = data[0];
+            int numbersToPop = data[1];
+            int mainNum = data[2];
+
+            for (int i = 0; i < numsToPush; i++)
+            {
+                int number = numbers[i];
+                if (number < minValue)
+                {
+                    minValue = number;
+                }
+                stack.Push(number);
+            }
+            for (int i = 0; i < numbersToPop; i++)
+            {
+                if (stack.Count > 0)
+                {
+                    stack.Pop();
+                }
+            }
+            if (stack.Count > 0)
+            {
+                if (stack.Contains(mainNum))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(minValue);
+                }
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
         }
     }
 }
