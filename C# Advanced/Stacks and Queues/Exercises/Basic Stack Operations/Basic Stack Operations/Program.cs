3ae2780563f0a70946cadceaaa88ece3b4fde9ef
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-09-24 21:55:42 +0300

    New Structure

---
+++ C# Advanced/Stacks and Queues/Exercises/Basic Stack Operations/Basic Stack Operations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
