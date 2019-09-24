b94d9e2f6161feefd59022b29022b9ba9bbc4923
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-09-24 23:07:43 +0300

    Another Task for the Course

---
+++ C# Advanced/Stacks and Queues/Exercises/Basic Queue Operations/Basic Queue Operations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int numbersToPush = data[0];
+            int numbersToPop = data[1];
+            int mainNumber = data[2];
+            int minValue = int.MaxValue;
+            var queue = new Queue<int>();
+
+            foreach (var item in numbers)
+            {
+                if (item <= minValue)
+                {
+                    minValue = item;
+                }
+                queue.Enqueue(item);
+            }
+            for (int i = 0; i < numbersToPop; i++)
+            {
+                if (queue.Count > 0)
+                {
+                    queue.Dequeue();
+                }
+            }
+            if (queue.Count > 0)
+            {
+                if (queue.Contains(mainNumber))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(minValue);
+                }
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
