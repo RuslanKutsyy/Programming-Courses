620228f4fcc837f19ec0255a7a81d2cb6ec7586e
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-16 00:59:52 +0300

    Record Unique Names Resolved

---
+++ C# Advanced/Sets and Dictionaries/Lab/Record Unique Names/Record Unique Names/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < number; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
