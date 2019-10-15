a456a02e9b53cbf568e7ce11249eaf569400a557
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-15 23:40:03 +0300

    Count Same Values in Array Resolved

---
+++ C# Advanced/Sets and Dictionaries/Lab/Count Same Values in Array/Count Same Values in Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Count_Same_Values_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Dictionary<double, int> dict = new Dictionary<double, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!dict.ContainsKey(nums[i]))
+                {
+                    dict.Add(nums[i], 1);
+                }
+                else
+                {
+                    dict[nums[i]]++;
+                }
+            }
+
+            foreach (var pair in dict)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value} times");
+            }
+
+        }
+    }
+}
