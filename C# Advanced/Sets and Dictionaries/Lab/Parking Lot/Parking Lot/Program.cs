60459c97968a02badc55b9abf1b0f9a92718e25a
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-16 01:12:11 +0300

    Parking Lot Resovled

---
+++ C# Advanced/Sets and Dictionaries/Lab/Parking Lot/Parking Lot/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            HashSet<string> cars = new HashSet<string>();
+
+            while (command != "END")
+            {
+                var data = command.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string direction = data[0];
+                string carNumber = data[1];
+
+                if (direction == "IN")
+                {
+                    if (!cars.Contains(carNumber))
+                    {
+                        cars.Add(carNumber);
+                    }
+                }
+                else if (direction == "OUT")
+                {
+                    if (cars.Contains(carNumber))
+                    {
+                        cars.Remove(carNumber);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (cars.Count > 0)
+            {
+                foreach (var carNumber in cars)
+                {
+                    Console.WriteLine(carNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
