633bfcaa8835585901a48d35240888fc143a416a
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-16 15:55:22 +0300

    Unique Usernames Resolved

---
+++ C# Advanced/Sets and Dictionaries/Exercises/Unique Usernames/Unique Usernames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int names = int.Parse(Console.ReadLine());
+            HashSet<string> usernames = new HashSet<string>();
+
+            for (int i = 0; i < names; i++)
+            {
+                string name = Console.ReadLine();
+                usernames.Add(name);
+            }
+
+            foreach (var name in usernames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
