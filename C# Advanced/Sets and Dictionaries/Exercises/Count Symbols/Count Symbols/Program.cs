c41ad4b061e94dad49f6a225a49e4063660b4a90
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-17 01:12:10 +0300

    Count Symbols Resolved

---
+++ C# Advanced/Sets and Dictionaries/Exercises/Count Symbols/Count Symbols/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine().ToCharArray();
+            Dictionary<char, int> results = new Dictionary<char, int>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!results.ContainsKey(words[i]))
+                {
+                    results.Add(words[i], 1);
+                }
+                else
+                {
+                    results[words[i]]++;
+                }
+            }
+
+            foreach (var item in results.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
