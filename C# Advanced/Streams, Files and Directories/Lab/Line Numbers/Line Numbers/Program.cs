d89307f9b466a39005c7197c3e8cf31d4c21a5c2
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-17 23:10:34 +0300

    Line Numbers Resolved

---
+++ C# Advanced/Streams, Files and Directories/Lab/Line Numbers/Line Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader(Environment.CurrentDirectory + "\\Input.txt"))
+            {
+                int counter = 1;
+                string line = reader.ReadLine();
+
+                using (var writer = new StreamWriter(Environment.CurrentDirectory + "\\Output.txt"))
+                {
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
