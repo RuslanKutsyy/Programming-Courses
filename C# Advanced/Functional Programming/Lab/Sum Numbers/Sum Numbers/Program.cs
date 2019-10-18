9a58fdeabde90bbf0e396138a9931690d196e8ae
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-18 22:19:36 +0300

    Sum Numbers Resolved

---
+++ C# Advanced/Functional Programming/Lab/Sum Numbers/Sum Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(", ").Select(x=>int.Parse(x)).ToArray();
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
