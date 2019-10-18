c92db7b77e641b022770f3e3e31f174b59930032
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-18 22:49:00 +0300

    Add VAT_resolved

---
+++ C# Advanced/Functional Programming/Lab/Add VAT/Add VAT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, double> doublesPlusVAT = x => double.Parse(x) * 1.2;
+
+            var prices = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(doublesPlusVAT).ToArray();
+
+            foreach (var price in prices)
+            {
+                Console.WriteLine($"{price:F2}");
+            }
+        }
+    }
+}
