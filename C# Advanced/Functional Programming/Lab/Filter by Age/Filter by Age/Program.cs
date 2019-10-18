4edfc0864302093950d422e590e7ff44813eb75b
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-19 00:47:28 +0300

    Filter by Age_Unresolved

---
+++ C# Advanced/Functional Programming/Lab/Filter by Age/Filter by Age/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Filter_by_Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var data = Console.ReadLine().Split().ToArray();
+                string name = data[0];
+                int age = int.Parse(data[1]);
+                dict.Add(name, age);
+            }
+
+            string condition = Console.ReadLine();
+            int conditionAge = int.Parse(Console.ReadLine());
+            string format = Console.ReadLine();
+
+        }
+    }
+}
