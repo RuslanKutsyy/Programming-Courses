2fe868a254524d264d5ded98c0f4975e34876818
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-18 16:28:16 +0300

    Sort Even Numbers Resolved

---
+++ C# Advanced/Functional Programming/Lab/Sort Even Numbers/Sort Even Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var newNums = numbers.OrderBy(x => x).Where(x => x % 2 == 0);
+
+            Console.WriteLine(string.Join(", ", newNums));
+        }
+    }
+}
