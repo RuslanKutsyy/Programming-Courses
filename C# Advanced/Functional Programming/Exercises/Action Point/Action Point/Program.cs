89969d9d7a0541adaaa1eb69599f7d302ba937a6
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-22 14:21:52 +0300

    Action Point Resolved

---
+++ C# Advanced/Functional Programming/Exercises/Action Point/Action Point/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Action_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine().Split().ToArray();
+
+            Action<string> Printer = x => Console.WriteLine(x);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Printer(words[i]);
+            }
+        }
+    }
+}
