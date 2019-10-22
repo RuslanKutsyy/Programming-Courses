188885bdf71c39d937a11973e02285b6bbb364be
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-22 21:50:05 +0300

    Predicate for Names Resolved

---
+++ C# Advanced/Functional Programming/Exercises/Predicate for Names/Predicate for Names/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Predicate_for_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nameLength = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split().ToArray();
+            Predicate<string> check = x => x.Length <= nameLength;
+
+            foreach (var name in names)
+            {
+                if (check(name))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
