f741875a01b65feee44f3c24c91588f03cc33a48
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-22 14:33:13 +0300

    Knights of Honor Resolved

---
+++ C# Advanced/Functional Programming/Exercises/Knights of Honor/Knights of Honor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine().Split().ToArray();
+            Action<string> Append = name => Console.WriteLine($"Sir {name}");
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Append(names[i]);
+            }
+        }
+    }
+}
