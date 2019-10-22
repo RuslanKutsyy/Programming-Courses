7f512825fd41b640c606109aa061c489b88727e9
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-22 21:07:24 +0300

    Applied Arithmetics Resolved

---
+++ C# Advanced/Functional Programming/Exercises/Applied Arithmetics/Applied Arithmetics/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string command = Console.ReadLine();
+            Func<int, int> add = x => x += 1;
+            Func<int, int> multiply = x => x *= 2;
+            Func<int, int> subtract = x => x -= 1;
+            Action<List<int>> print = x => Console.WriteLine(string.Join(" ", x));
+
+            while (command != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                        {
+                            numbers = numbers.Select(add).ToList();
+                            break;
+                        }
+                    case "multiply":
+                        {
+                            numbers = numbers.Select(multiply).ToList();
+                            break;
+                        }
+                    case "subtract":
+                        {
+                            numbers = numbers.Select(subtract).ToList();
+                            break;
+                        }
+                    case "print":
+                        {
+                            print(numbers);
+                            break;
+                        }
+                    default:
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
