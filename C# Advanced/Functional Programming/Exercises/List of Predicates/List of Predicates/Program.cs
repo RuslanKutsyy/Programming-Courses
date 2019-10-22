d19064653a62ecf59ad796137220dc6339d00459
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-22 23:41:45 +0300

    List of Predicates Resolved

---
+++ C# Advanced/Functional Programming/Exercises/List of Predicates/List of Predicates/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace List_of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            var dividers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Func<int, bool> check = x =>
+            {
+                int count = 0;
+                for (int i = 0; i < dividers.Length; i++)
+                {
+                    if (x % dividers[i] == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == dividers.Length)
+                {
+                    return true;
+                }
+                else
+                    return false;
+            };
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (check(i))
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
