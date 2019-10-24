c91b2e0d7cf1e5de879c4416d402c81379f5ed6f
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-25 01:39:29 +0300

    Speed_Racing Unresolved

---
+++ C# Advanced/Defining Classes/Exercises/Speed_Racing/Speed_Racing/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Speed_Racing
+{
+    class Car
+    {
+        private string model { get; set; }
+        private double fuelAmount { get; set; }
+        private double fuelConsumptionPerKilometer { get; set; }
+        private double travelledDistance { get; set; }
+
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        public double FuelAmount
+        {
+            get { return this.fuelAmount; }
+            set { this.fuelAmount = value; }
+        }
+
+        public double FuelConsumptionPerKilometer
+        {
+            get { return this.fuelConsumptionPerKilometer; }
+            set { this.fuelConsumptionPerKilometer = value; }
+        }
+
+        public double TravelledDistance
+        {
+            get { return this.travelledDistance; }
+            set { this.travelledDistance = value; }
+        }
+
+        public Car(string model, double fuelAmount, double fuelConsumptionFor1km, double travelledDistance)
+        {
+            this.model = model;
+            this.fuelAmount = fuelAmount;
+            this.fuelConsumptionPerKilometer = fuelConsumptionFor1km;
+        }
+
+        public static void Drive(double carModel, double amountOfKm)
+        {
+
+        }
+    }
+}
