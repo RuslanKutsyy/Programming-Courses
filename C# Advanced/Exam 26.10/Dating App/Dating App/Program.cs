ae3bc6ae0b4ba19e9556c9533ad2297a62426daa
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-26 21:35:18 +0300

    Dating_App(to be tested)

--- C# Advanced/Exam 26.10/Dating App/Dating App/Program.cs
+++ C# Advanced/Exam 26.10/Dating App/Dating App/Program.cs
@@ -8,96 +8,91 @@ namespace Dating_App
     {
         static void Main(string[] args)
         {
-            var malesText = Console.ReadLine().Split().Select(int.Parse);
-            var femalesText = Console.ReadLine().Split().Select(int.Parse);
-            Queue<int> males = new Queue<int>(malesText.Reverse());
-            Queue<int> females = new Queue<int>(femalesText);
+            var maleText = Console.ReadLine().Split().Select(int.Parse);
+            var femaleText = Console.ReadLine().Split().Select(int.Parse);
+            Queue<int> males = new Queue<int>();
+            Queue<int> females = new Queue<int>();
             int matchCount = 0;
 
-            while (males.Count != 0)
+            foreach (var item in maleText.Reverse())
             {
-                int currentMale = males.Peek();
-                if (currentMale <= 0)
+                males.Enqueue(item);
+            }
+            foreach (var item in femaleText)
+            {
+                females.Enqueue(item);
+            }
+
+            while (males.Count != 0 && females.Count != 0)
+            {
+                int currentFemale = females.Peek();
+
+                if (currentFemale == 0 || currentFemale % 25 == 0)
                 {
-                    males.Dequeue();
+                    females.Dequeue();
+
+                    if (currentFemale % 25 == 0 && currentFemale != 0)
+                    {
+                        females.Dequeue();
+                    }
                     continue;
                 }
-                if (currentMale % 25 == 0)
+
+                for (int i = 0; i < males.Count; i++)
                 {
-                    males.Dequeue();
-                    if (males.Count != 0)
+                    int currentMale = males.Peek();
+
+                    if (currentMale == 0 || currentMale % 25 == 0)
                     {
                         males.Dequeue();
+                        if ((double)currentMale % 25 == 0 && currentMale != 0)
+                        {
+                            males.Dequeue();
+                        }
+                        i = -1;
+                        continue;
                     }
-                }
-                if (females.Count != 0)
-                {
-                    int currentFemale = 0;
-                    for (int j = 0; j < females.Count; j++)
+
+                    if (currentFemale == currentMale)
                     {
-                        if (j == 0)
-                        {
-                            currentFemale = females.Peek();
-                        }
-                        if (currentFemale <= 0)
-                        {
-                            females.Dequeue();
-                            continue;
-                        }
-                        if (currentFemale % 25 == 0)
-                        {
-                            females.Dequeue();
-                            if (females.Count != 0)
-                            {
-                                females.Dequeue();
-                                break;
-                            }
-                        }
-                        if (currentMale <= 0)
-                        {
-                            males.Dequeue();
-                        }
-                        if (currentMale == currentFemale)
-                        {
-                            males.Dequeue();
-                            females.Dequeue();
-                            matchCount++;
-                            break;
-                        }
-                        else if (currentMale != currentFemale)
-                        {
-                            females.Dequeue();
-                            currentMale /= 2;
-                            males.Dequeue();
-                            males.Enqueue(currentMale);
-                            j = 0;
-                        }
+                        matchCount++;
+                        females.Dequeue();
+                        males.Dequeue();
+                        break;
                     }
-                }
-                if (males.Count == 0 || females.Count == 0)
-                {
-                    break;
+                    else
+                    {
+                        females.Dequeue();
+                        males.Dequeue();
+                        males.Enqueue(currentMale / 2);
+                        Queue<int> temp = males;
+                        males = new Queue<int>();
+                        foreach (var item in temp.Reverse())
+                        {
+                            males.Enqueue(item);
+                        }
+                        break;
+                    }
                 }
             }
 
             Console.WriteLine($"Matches: {matchCount}");
-            if (males.Count == 0)
+            if (males.Count != 0)
             {
-                Console.WriteLine("Males left: none");
-
+                Console.WriteLine($"Males left: {string.Join(", ", males)}");
             }
             else
             {
-                Console.WriteLine($"Males left: {males.Count}");
+                Console.WriteLine($"Males left: none");
             }
 
-            if (females.Count == 0)
-            {
-                Console.WriteLine("Females left: none");
-            }
-            else
+            if (females.Count != 0)
             {
                 Console.WriteLine($"Females left: {string.Join(", ", females)}");
+            }
+            else
+            {
+                Console.WriteLine("Females left: none");
             }
         }
     }
