450c6c2fa446b04bff0c65a615bb6dc8e64d2e60
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-29 17:50:43 +0200

    Point in Rectangle Unfinished

---
+++ C# OOP/Working with Abstraction/Lab/Point in Rectangle/Point in Rectangle/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Point_in_Rectangle
+{
+    class Point
+    {
+        private int x;
+        private int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ C# OOP/Working with Abstraction/Lab/Point in Rectangle/Point in Rectangle/Rectangle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Point_in_Rectangle
+{
+    class Rectangle
+    {
+    }
+}
