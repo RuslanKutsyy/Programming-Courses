5c27ff883d03892140c83f6a501f1a70e5f57e07
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-10-29 15:30:18 +0200

    Rhombus of Stars Resolved

---
+++ C# OOP/Working with Abstraction/Lab/Rhombus of Stars/Rhombus of Stars/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Rhombus_of_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            for (int stCount = 1; stCount <= size; stCount++)
+            {
+                PrintRow(size, stCount);
+            }
+
+            for (int stCount = size - 1; stCount >= 1; stCount--)
+            {
+                PrintRow(size, stCount);
+            }
+        }
+
+        static void PrintRow(int figureSize, int starCount)
+        {
+            for (int i = 0; i < figureSize - starCount; i++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int i = 1; i < starCount; i++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine("*");
+
+        }
+    }
+}
