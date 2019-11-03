cfacb2c9f07e1247db22343931b1539786442ec7
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-11-04 01:56:18 +0200

    Car_Salesman 70/100

---
+++ C# OOP/Working with Abstraction/Exercises/Car_Salesman/Car_Salesman/Car.cs
@@ -0,0 +1,112 @@
+﻿using System.Text;
+
+namespace Car_Salesman
+{
+    class Car
+    {
+        private string model;
+        private Engine engine;
+        private double weight;
+        private string color;
+
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        public Engine Engine
+        {
+            get { return this.engine; }
+            set { this.engine = value; }
+        }
+
+        public double Weight
+        {
+            get { return this.weight; }
+            set { this.weight = value; }
+        }
+
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+
+
+        public Car() : this(null, null, 0 , null)
+        {
+        }
+
+        public Car(string model, Engine engine) : this(model, engine, 0, null)
+        {
+            this.model = model;
+            this.engine = engine;
+        }
+
+        public Car(string model, Engine engine, double weight) : this(model, engine, weight, null)
+        {
+            this.model = model;
+            this.engine = engine;
+            this.weight = weight;
+        }
+        public Car(string model, Engine engine, string color) : this(model, engine, 0, color)
+        {
+            this.model = model;
+            this.engine = engine;
+            this.color = color;
+        }
+
+        public Car(string model, Engine engine, double weight, string color)
+        {
+            this.model = model;
+            this.engine = engine;
+            this.weight = weight;
+            this.color = color;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder($"{this.model}:\n");
+            sb.AppendLine($"  {this.Engine.Model}:");
+            sb.AppendLine($"    Power: {this.Engine.Power}");
+            if (this.Engine.Displacement == 0)
+            {
+                sb.AppendLine($"    Displacement: n/a");
+            }
+            else
+            {
+                sb.AppendLine($"    Displacement: {this.Engine.Displacement}");
+            }
+
+            if (this.Engine.Efficiency == null)
+            {
+                sb.AppendLine($"    Efficiency: n/a");
+            }
+            else
+            {
+                sb.AppendLine($"    Efficiency: {this.Engine.Efficiency}");
+            }
+
+            if (this.weight == 0)
+            {
+                sb.AppendLine($"  Weight: n/a");
+            }
+            else
+            {
+                sb.AppendLine($"  Weight: {this.weight}");
+            }
+
+            if (this.color == null)
+            {
+                sb.Append($"  Color: n/a");
+            }
+            else
+            {
+                sb.Append($"  Color: {this.color}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ C# OOP/Working with Abstraction/Exercises/Car_Salesman/Car_Salesman/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Car_Salesman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfEngines = int.Parse(Console.ReadLine());
+            Dictionary<string, Engine> engines = new Dictionary<string, Engine>();
+            string model;
+            int power = 0;
+            int displacement = 0;
+            string efficiency = null;
+
+
+            for (int i = 0; i < numOfEngines; i++)
+            {
+                var engineData = Console.ReadLine().Split().ToArray();
+                model = engineData[0];
+                power = int.Parse(engineData[1]);
+                if (engineData.Length == 3)
+                {
+                    bool isInt = int.TryParse(engineData[2], out displacement);
+                    if (!isInt)
+                    {
+                        efficiency = engineData[2];
+                    }
+                }
+                else if (engineData.Length == 4)
+                {
+                    displacement = int.Parse(engineData[2]);
+                    efficiency = engineData[3];
+                }
+                Engine engine = new Engine(model, power, displacement, efficiency);
+                engines.Add(model, engine);
+            }
+
+            int numOfCars = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+            string carModel;
+            Engine carEngine = null;
+            int weight = 0;
+            string color = null;
+
+            for (int i = 0; i < numOfCars; i++)
+            {
+                var carData = Console.ReadLine().Split().ToArray();
+
+                carModel = carData[0];
+                carEngine = engines[carData[1]];
+                weight = 0;
+                color = null;
+
+                if (carData.Length == 3)
+                {
+                    bool isInt = int.TryParse(carData[2], out weight);
+                    if (!isInt)
+                    {
+                        color = carData[2];
+                    }
+                }
+                else if (carData.Length == 4)
+                {
+                    bool isInt = int.TryParse(carData[2], out weight);
+                    if (isInt)
+                    {
+                        color = carData[3];
+                    }
+                    else
+                    {
+                        color = carData[2];
+                        weight = int.Parse(carData[3]);
+                    }
+                }
+
+                Car car = new Car(carModel, carEngine, weight, color);
+
+                cars.Add(car);
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.ToString());
+            }
+        }
+    }
+}
