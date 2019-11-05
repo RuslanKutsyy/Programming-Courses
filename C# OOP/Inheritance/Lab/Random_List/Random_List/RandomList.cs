b08557e3606bbbcb3725a4c37fd34685fa5ea7a2
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-11-05 23:47:16 +0200

    Random_List Resolved

---
+++ C# OOP/Inheritance/Lab/Random_List/Random_List/RandomList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    class RandomList : List<string>
+    {
+        public void RandomString()
+        {
+            Random num = new Random();
+            int element = num.Next(this.Count);
+            Console.WriteLine(this[element]);
+            this.RemoveAt(element);            
+        }
+    }
+}
