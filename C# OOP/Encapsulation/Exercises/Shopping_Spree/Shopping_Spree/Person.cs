a7da5600c04a714a97dd2a5b059b89720ff2a6f7
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-11-18 18:55:33 +0200

    Shopping_Spree Unfinished

---
+++ C# OOP/Encapsulation/Exercises/Shopping_Spree/Shopping_Spree/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopping_Spree
+{
+    class Person
+    {
+        private string name;
+        private double money;
+        private List<Product> bag;
+
+        public Person(string name, double money)
+        {
+            this.Name = name;
+            this.Money = money;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+
+        public double Money
+        {
+            get { return this.money; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+
+        public List<Product> Bag
+        {
+            get { return this.bag; }
+            set
+            {
+                if (true)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ C# OOP/Encapsulation/Exercises/Shopping_Spree/Shopping_Spree/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Shopping_Spree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
