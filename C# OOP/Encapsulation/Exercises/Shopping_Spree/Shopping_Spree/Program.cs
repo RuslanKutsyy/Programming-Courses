dae7c5e122a1894340249d98a2f6283a2e872b70
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-11-19 23:33:56 +0200

    Shopping_Spree Resolved

--- C# OOP/Encapsulation/Exercises/Shopping_Spree/Shopping_Spree/Program.cs
+++ C# OOP/Encapsulation/Exercises/Shopping_Spree/Shopping_Spree/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 
 namespace Shopping_Spree
 {
@@ -7,30 +8,72 @@ namespace Shopping_Spree
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-            ShopQueue Queue = new ShopQueue();
-            Queue.AddToQueue(input);
-
-            string productsData = Console.ReadLine();
-            ProductCart Cart = new ProductCart();
-            Cart.AddToCart(productsData);
-
-            foreach (var person in Queue)
+            try
             {
-                for (int cartItem = 0; cartItem < Queue.Count; cartItem++)
+                string input = Console.ReadLine();
+                ShopQueue Queue = new ShopQueue();
+                Queue.AddToQueue(input);
+
+                string productsData = Console.ReadLine();
+                ProductCart Cart = new ProductCart();
+                Cart.AddToCart(productsData);
+
+                string cmd = Console.ReadLine();
+
+                while (cmd != "END")
                 {
-                    if (person.Money >= Cart[cartItem].Cost)
+                    var data = cmd.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    string name = data[0];
+                    string prodName = data[1];
+
+                    foreach (var person in Queue.Queue.Where(person => person.Name == name))
                     {
-                        person.Bag.Add(Cart[cartItem]);
-                        person.Money -= Cart[cartItem].Cost;
-                        Console.WriteLine($"{person.Name} bought {Cart[cartItem].Name}");
+                        foreach (var prod in Cart.Cart.Where(x => x.Name == prodName))
+                        {
+                            if (prod.Cost <= person.Money)
+                            {
+                                person.Bag.Add(prod);
+                                person.Money -= prod.Cost;
+                                Console.WriteLine($"{name} bought {prodName}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{name} can't afford {prodName}");
+                            }
+                        }
+                    }
+                    cmd = Console.ReadLine();
+                }
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var person in Queue.Queue)
+                {
+                    if (person.Bag.Count > 0)
+                    {
+                        sb.Append($"{person.Name} - ");
+                        for (int i = 0; i < person.Bag.Count; i++)
+                        {
+                            sb.Append($"{person.Bag[i].Name}");
+                            if (i != person.Bag.Count - 1)
+                            {
+                                sb.Append(", ");
+                            }
+                        }
+                        sb.AppendLine();
+                    }
+                    else
+                    {
+                        sb.Append($"{person.Name} - Nothing bought");
                     }
                 }
+
+                Console.WriteLine(sb);
             }
-
-
-
-            
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
