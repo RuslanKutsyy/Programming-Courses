604631a120c3050a35d17c53bee104e3d81fec4f
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-11-23 00:11:03 +0200

    Pizza_Calories 80/100

---
+++ C# OOP/Encapsulation/Exercises/Pizza_Calories/Pizza_Calories/Dough.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Pizza_Calories
+{
+    public class Dough
+    {
+        public const int CaloriesPerGramm = 2;
+        internal string flour;
+        internal string bakingTechnique;
+        internal double weight;
+
+        public Dough(string flour, string bakingTechnique, double weight)
+        {
+            this.Flour = flour;
+            this.BakingTechnique = bakingTechnique;
+            this.Weight = weight;
+        }
+
+        private string Flour
+        {
+            get { return this.flour; }
+            set
+            {
+                value = ChangeFormat(value);
+                if (value != "White" && value != "Wholegrain")
+                {
+                    throw new Exception("Invalid type of dough.");
+                }
+                this.flour = value;
+            }
+        }
+
+        private string BakingTechnique
+        {
+            get { return this.bakingTechnique; }
+            set
+            {
+                value = ChangeFormat(value);
+                if (value[0] != char.ToUpper(value[0]))
+                {
+                    value = value.Replace(value[0], char.ToUpper(value[0]));
+                }
+
+                if (value != "Crispy" && value != "Chewy" && value != "Homemade")
+                {
+                    throw new Exception("Invalid type of dough.");
+                }
+                this.bakingTechnique = value;
+            }
+        }
+
+        private double Weight
+        {
+            get { return this.weight; }
+            set
+            {
+                if (value < 1 || value > 200)
+                {
+                    throw new Exception("Dough weight should be in the range [1..200].");
+                }
+                this.weight = value;
+            }
+        }
+
+        private double GetModifier(string doughType)
+        {
+            switch (doughType)
+            {
+                case "White": return 1.5;
+                case "Wholegrain": return 1.0;
+                case "Crispy": return 0.9;
+                case "Chewy": return 1.1;
+                case "Homemade": return 1.0;
+                default:
+                    return -1;
+            }
+        }
+
+        public double GetCalories()
+        {
+            double calories = CaloriesPerGramm * this.Weight * GetModifier(this.Flour) * GetModifier(this.BakingTechnique);
+
+            return calories;
+        }
+
+        private string ChangeFormat(string value)
+        {
+            value = value.ToLower();
+            value = value.Replace(value[0], char.ToUpper(value[0]));
+
+            return value;
+        }
+    }
+}
---
+++ C# OOP/Encapsulation/Exercises/Pizza_Calories/Pizza_Calories/Pizza.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizza_Calories
+{
+    class Pizza
+    {
+        private string name;
+        private Dough dough;
+        private List<Topping> toppings;
+
+        public Pizza(string pizzaName)
+        {
+            this.Name = pizzaName;
+            this.toppings = new List<Topping>();
+            //this.dough = new Dough(doughType, bakingTechnique, weight);
+            //this.toppings = new List<Topping>();
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) && value.Length > 15)
+                {
+                    throw new Exception("Pizza name should be between 1 and 15 symbols.");
+                }
+                this.name = value;
+            }
+        }
+
+        private List<Topping> Toppings
+        {
+            get { return this.toppings; }
+            set
+            {
+                if (value.Count > 10)
+                {
+                    throw new Exception("Number of toppings should be in range [0..10].");
+                }
+                this.toppings = value;
+            }
+        } 
+
+        public Dough Dough
+        {
+            get { return this.dough; }
+            set
+            {
+                this.dough = value ?? throw new Exception("Invalid type of dough.");
+            }
+        }
+
+        public int NumberOfToppings()
+        {
+            return this.toppings.Count;
+        }
+
+        public double GetTotalCalories()
+        {
+            double calories = this.dough.GetCalories();
+            foreach (var topping in toppings)
+            {
+                calories += topping.GetCalories();
+            }
+
+            return calories;
+        }
+
+        public void AddToppings(Topping topping)
+        {
+            if (this.Toppings.Count + 1 > 10)
+            {
+                throw new Exception("Number of toppings should be in range [0..10].");
+            }
+            this.Toppings.Add(topping);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{this.Name} - {GetTotalCalories():F2} Calories.");
+
+            return sb.ToString();
+        }
+    }
+}
