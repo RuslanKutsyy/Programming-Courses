14a14d8cbb61865dd0c65e057653d52f6b16f630
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-11-24 00:34:56 +0200

    Ferrari Uploaded

--- C# OOP/Interfaces and Abstraction/Exercises/Define_an_Interface_IPerson/Define_an_Interface_IPerson/Citizen.cs
+++ C# OOP/Interfaces and Abstraction/Exercises/Define_an_Interface_IPerson/Define_an_Interface_IPerson/Citizen.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace PersonInfo
 {
-    class Citizen : IPerson, IIdentifiable, IBirthable
+    public class Citizen : IPerson, IIdentifiable, IBirthable
     {
         public string Name { get; set; }
         public int Age { get; set; }
---
+++ C# OOP/Interfaces and Abstraction/Exercises/Ferrari/Ferrari/Ferrari.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ferrari
+{
+    public class Ferrari : ICar
+    {
+        public string Model { get; set; }
+        public string Driver { get; set; }
+
+        public Ferrari(string name)
+        {
+            this.Model = "488-Spider";
+            this.Driver = name;
+        }
+
+        public string Brake()
+        {
+            string output = "Brakes!";
+            return output;            
+        }
+
+        public string Gas()
+        {
+            string output = "Gas!";
+            return output;
+        }
+
+        public override string ToString()
+        {
+            string output = $"{this.Model}/{this.Brake()}/{this.Gas()}/{this.Driver}";
+            return output.ToString();
+        }
+    }
+}
