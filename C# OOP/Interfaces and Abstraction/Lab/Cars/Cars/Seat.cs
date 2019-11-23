97dd7a1748a43e5b6504580ba7cdf37a2e2d5811
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-11-23 20:46:49 +0200

    Cars Uploaded

---
+++ C# OOP/Interfaces and Abstraction/Lab/Cars/Cars/Seat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    class Seat : ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+
+        public string Start()
+        {
+            string text = "Engine start";
+            return text;
+        }
+
+        public string Stop()
+        {
+            string text = "Breaaak!";
+            return text;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} Seat {this.Model}");
+            sb.AppendLine(this.Start());
+            sb.Append(this.Stop());
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ C# OOP/Interfaces and Abstraction/Lab/Cars/Cars/Tesla.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    class Tesla : ICar, IElectricCar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Batteries { get ; set; }
+
+        public Tesla(string model, string color, int batteries)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Batteries = batteries;
+        }
+        public string Start()
+        {
+            string text = "Engine start";
+
+            return text;
+        }
+
+        public string Stop()
+        {
+            string text = "Breaaak!";
+            return text;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} Tesla {this.Model} with {this.Batteries} Batteries");
+            sb.AppendLine(this.Start());
+            sb.Append(this.Stop());
+
+            return sb.ToString();
+        }
+    }
+}
