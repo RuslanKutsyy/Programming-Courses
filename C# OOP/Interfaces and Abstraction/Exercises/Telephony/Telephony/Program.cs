59b2f5ad275fef16237b882797c65946be16a857
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-11-24 01:25:03 +0200

    Telephony Resolved

---
+++ C# OOP/Interfaces and Abstraction/Exercises/Telephony/Telephony/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Telephony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phoneNumbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var webSites = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var smartphone = new Smartphone();
+
+            foreach (var number in phoneNumbers)
+            {
+                smartphone.Call(number);
+            }
+
+            foreach (var website in webSites)
+            {
+                smartphone.Browse(website);
+            }
+        }
+    }
+}
