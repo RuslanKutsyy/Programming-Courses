7339fbffa5a569333e9129630bfeae46bc0e417b
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-11-24 03:18:27 +0200

    Border_Control Unfinished

---
+++ C# OOP/Interfaces and Abstraction/Exercises/Border_Control/Border_Control/Citizen.cs
@@ -0,0 +1,16 @@
+﻿namespace Border_Control
+{
+    public class Citizen : IIdentifiable
+    {
+        public string Id { get; set; }
+        string Name { get; set; }
+        int Age { get; set; }
+
+        public Citizen(string id, string name, int age)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
---
+++ C# OOP/Interfaces and Abstraction/Exercises/Border_Control/Border_Control/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Border_Control
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            City city = new City();
+            string cmd = Console.ReadLine();
+
+            while (cmd != "End")
+            {
+                var data = cmd.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (data.Length == 2)
+                {
+                    string model = data[0];
+                    string id = data[1];
+
+                    Robot robot = new Robot(model, id);
+
+                    city.Robots.Add(robot);
+                }
+                else if (data.Length == 3)
+                {
+                    string name = data[0];
+                    int age = int.Parse(data[1]);
+                    string id = data[2];
+
+                    Citizen citizen = new Citizen(id, name, age);
+
+                    city.Citizens.Add(citizen);
+                }
+
+                cmd = Console.ReadLine();
+            }
+
+            string fakeIDs = Console.ReadLine();
+
+            Console.Write(city.FakeCheck(fakeIDs));
+        }
+    }
+}
