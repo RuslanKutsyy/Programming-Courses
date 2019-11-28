5abb68473d933cc593300401ec97f404f1a70902
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-11-28 23:59:14 +0200

    Vehicles Resolved

---
+++ C# OOP/Polymorphism/Exercises/Vehicles/Vehicles/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public class Car
+    {
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; }
+
+        public Car(double fuelQuantity, double fuelConsumption)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public virtual void DriveCar(double km)
+        {
+            double neededFuel = (this.FuelConsumption + 0.9) * km;
+
+            if (neededFuel < this.FuelQuantity)
+            {
+                this.FuelQuantity -= neededFuel;
+                Console.WriteLine($"Car travelled {km} km");
+            }
+            else
+            {
+                Console.WriteLine("Car needs refueling");
+            }
+        }
+
+        public virtual void Refuel(double litters)
+        {
+            this.FuelQuantity += litters;
+        }
+    }
+}
---
+++ C# OOP/Polymorphism/Exercises/Vehicles/Vehicles/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Vehicles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var carData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            double fuelQuantity = double.Parse(carData[1]);
+            double fuelConsumption = double.Parse(carData[2]);
+            Car car = new Car(fuelQuantity, fuelConsumption);
+
+            var truckData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            fuelQuantity = double.Parse(truckData[1]);
+            fuelConsumption = double.Parse(truckData[2]);
+            Truck truck = new Truck(fuelQuantity, fuelConsumption);
+
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                var cmd = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string command = cmd[0];
+
+                if (command == "Drive")
+                {
+                    string type = cmd[1];
+                    double km = double.Parse(cmd[2]);
+
+                    if (type == "Car")
+                    {
+                        car.DriveCar(km);
+                    }
+                    else if (type == "Truck")
+                    {
+                        truck.DriveCar(km);
+                    }
+                }
+                else if (command == "Refuel")
+                {
+                    string type = cmd[1];
+                    double litters = double.Parse(cmd[2]);
+
+                    if (type == "Car")
+                    {
+                        car.Refuel(litters);
+                    }
+                    else if (type == "Truck")
+                    {
+                        truck.Refuel(litters);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Car: {car.FuelQuantity:F2}");
+            Console.WriteLine($"Truck: {truck.FuelQuantity:F2}");
+        }
+    }
+}
