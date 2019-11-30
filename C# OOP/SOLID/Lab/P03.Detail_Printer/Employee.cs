13066bcdd7aff6d5c1329c2a352d25807b7f2b56
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-11-30 20:33:38 +0200

    DetailsPrinter Refactoring

--- C# OOP/SOLID/Lab/P03.Detail_Printer/Employee.cs
+++ C# OOP/SOLID/Lab/P03.Detail_Printer/Employee.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace P03.DetailPrinter
 {
-    public class Employee
+    public class Employee : IEmployee
     {
         public Employee(string name)
         {
@@ -12,5 +12,10 @@ namespace P03.DetailPrinter
         }
 
         public string Name { get; set; }
+
+        public virtual void Print()
+        {
+            Console.WriteLine(this.Name);
+        }
     }
 }
--- C# OOP/SOLID/Lab/P03.Detail_Printer/Manager.cs
+++ C# OOP/SOLID/Lab/P03.Detail_Printer/Manager.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace P03.DetailPrinter
 {
-    public class Manager : Employee
+    public class Manager : Employee, IEmployee
     {
         public Manager(string name, ICollection<string> documents) : base(name)
         {
@@ -12,5 +12,11 @@ namespace P03.DetailPrinter
         }
 
         public IReadOnlyCollection<string> Documents { get; set; }
+
+        public override void Print()
+        {
+            Console.WriteLine(this.Name);
+            Console.WriteLine(string.Join(Environment.NewLine, this.Documents));
+        }
     }
 }
