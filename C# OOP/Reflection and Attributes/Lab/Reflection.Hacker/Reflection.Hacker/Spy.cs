067016a959fe57f616efc1d9792b353d81da581b
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-12-01 19:25:49 +0200

    Reflection.Hacker - Resolve Stealer

---
+++ C# OOP/Reflection and Attributes/Lab/Reflection.Hacker/Reflection.Hacker/Spy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+using System.Linq;
+
+public class Spy
+{
+    public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+    {
+        Type classType = Type.GetType(investigatedClass);
+        FieldInfo[] classFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+        StringBuilder sb = new StringBuilder();
+
+        object instance = Activator.CreateInstance(classType, new object[] { });
+        sb.AppendLine($"Class under investigation: {investigatedClass}");
+
+        foreach (var field in classFields.Where(f => requestedFields.Contains(f.Name)))
+        {
+            sb.AppendLine($"{field.Name} = {field.GetValue(instance)}");
+        }
+
+        return sb.ToString().Trim();
+    }
+}
