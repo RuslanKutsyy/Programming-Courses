e6037f1cb6c8d610e4924bdd90b68edd25b43ca8
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-12-02 23:23:09 +0200

    Exercises Unfinished

---
+++ C# OOP/Reflection and Attributes/Exercises/ValidationAttributes/MyRangeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    class MyRangeAttribute : MyValidationAttribute
+    {
+
+        private int MinValue { get; }
+        private int MaxValue;
+
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.MinValue = minValue;
+            this.MaxValue = maxValue;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
