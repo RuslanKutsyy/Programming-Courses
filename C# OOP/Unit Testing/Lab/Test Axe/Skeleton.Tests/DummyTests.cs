911016ba96c3801576e161b66a67637b8b3f4f1d
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-12-05 13:12:25 +0200

    Updated tests

--- C# OOP/Unit Testing/Lab/Test Axe/Skeleton.Tests/DummyTests.cs
+++ C# OOP/Unit Testing/Lab/Test Axe/Skeleton.Tests/DummyTests.cs
@@ -4,7 +4,18 @@
 public class DummyTests
 {
     [Test]
-    public void Test1()
+    public void DummyLosesHealthIfAttacked()
+    {
+        Axe axe = new Axe(5, 10);
+        Dummy dummy = new Dummy(10, 10);
+
+        axe.Attack(dummy);
+
+        Assert.AreEqual(5, dummy.Health);
+    }
+
+
+    public void DeadDummyThrowsExceptionIfAttacked()
     {
 
     }
