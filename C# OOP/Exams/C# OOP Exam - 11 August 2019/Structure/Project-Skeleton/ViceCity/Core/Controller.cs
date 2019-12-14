fdb5d6fb441c166b372de412daaf7e6eafcefe4c
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-12-14 22:23:22 +0200

    Add Business Logic

---
+++ C# OOP/Exams/C# OOP Exam - 11 August 2019/Structure/Project-Skeleton/ViceCity/Core/Controller.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ViceCity.Core.Contracts;
+using ViceCity.Models.Guns;
+using ViceCity.Models.Guns.Contracts;
+using ViceCity.Models.Players;
+using ViceCity.Models.Players.Contracts;
+using System.Linq;
+using ViceCity.Models.Neghbourhoods;
+
+namespace ViceCity.Core
+{
+    public class Controller : IController
+    {
+        public IPlayer mainPlayer;
+        private List<IGun> guns;
+        private List<IPlayer> civilPlayers;
+        private GangNeighbourhood hood;
+
+        public Controller()
+        {
+            this.mainPlayer = new MainPlayer();
+            this.guns = new List<IGun>();
+            this.civilPlayers = new List<IPlayer>();
+            this.hood = new GangNeighbourhood();
+        }
+
+        public string AddGun(string type, string name)
+        {
+            switch (type)
+            {
+                case "Pistol":
+                    {
+                        Pistol pistol = new Pistol(name);
+                        this.guns.Add(pistol);
+
+                        return $"Successfully added {name} of type: {type}";
+                    }
+                case "Rifle":
+                    {
+                        Rifle rifle = new Rifle(name);
+                        this.guns.Add(rifle);
+
+                        return $"Successfully added {name} of type: {type}";
+                    }
+                default:
+                    return "Invalid gun type!";
+            }
+        }
+
+        public string AddGunToPlayer(string name)
+        {
+            if (this.guns.Count == 0)
+            {
+                return "There are no guns in the queue!";
+            }
+            else
+            {
+                if (name == "Vercetti")
+                {
+                    var weapon = this.guns[0];
+                    this.mainPlayer.GunRepository.Add(weapon);
+                    this.guns.Remove(weapon);
+
+                    return $"Successfully added {weapon.Name} to the Main Player: Tommy Vercetti";
+                }
+
+                if (!this.civilPlayers.Any(x => x.Name == name))
+                {
+                    return "Civil player with that name doesn't exists!";
+                }
+
+                var player = civilPlayers.Where(x => x.Name == name).FirstOrDefault();
+                var gun = this.guns[0];
+                player.GunRepository.Add(gun);
+                this.guns.Remove(gun);
+
+                return $"Successfully added {gun.Name} to the Civil Player: {player.Name}";
+            }
+        }
+
+        public string AddPlayer(string name)
+        {
+            CivilPlayer player = new CivilPlayer(name);
+            this.civilPlayers.Add(player);
+
+            return $"Successfully added civil player: {name}!";
+        }
+
+        public string Fight()
+        {
+            var mainPlayerHealthBeforeFight = this.mainPlayer.LifePoints;
+            var civilPlayersHealthBeforeFight = this.civilPlayers.Sum(p => p.LifePoints);
+
+            this.hood.Action(this.mainPlayer, this.civilPlayers);
+
+            if (mainPlayerHealthBeforeFight == this.mainPlayer.LifePoints &&
+                civilPlayersHealthBeforeFight == this.civilPlayers.Sum(p => p.LifePoints))
+            {
+                return "Everything is okay!";
+            }
+            else
+            {
+                return "A fight happened:" + Environment.NewLine
+                    + $"Tommy live points: {this.mainPlayer.LifePoints}!" + Environment.NewLine
+                    + $"Tommy has killed: {this.civilPlayers.Where(p => !p.IsAlive).Count()} players!" + Environment.NewLine
+                    + $"Left Civil Players: {this.civilPlayers.Where(p => p.IsAlive).Count()}!";
+            }
+
+        }
+    }
+}
---
+++ C# OOP/Exams/C# OOP Exam - 11 August 2019/Structure/Project-Skeleton/ViceCity/Core/Engine.cs
@@ -0,0 +1,59 @@
+﻿using ViceCity.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ViceCity.IO.Contracts;
+using ViceCity.IO;
+
+namespace ViceCity.Core
+{
+    public class Engine : IEngine
+    {
+        private IReader reader;
+        private IWriter writer;
+        private IController Controller;
+
+
+        public Engine()
+        {
+            this.reader = new Reader();
+            this.writer = new Writer();
+            this.Controller = new Controller();
+
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                var input = reader.ReadLine().Split();
+                if (input[0] == "Exit")
+                {
+                    Environment.Exit(0);
+                }
+                try
+                {
+                    if (input[0] == "AddPlayer")
+                    {
+                        this.writer.WriteLine(this.Controller.AddPlayer(input[1]));
+                    }
+                    else if (input[0] == "AddGun")
+                    {
+                        this.writer.WriteLine(this.Controller.AddGun(input[1], input[2]));
+                    }
+                    else if (input[0] == "AddGunToPlayer")
+                    {
+                        this.writer.WriteLine(this.Controller.AddGunToPlayer(input[1]));
+                    }
+                    else if (input[0] == "Fight")
+                    {
+                        this.writer.WriteLine(this.Controller.Fight());
+                    }            
+                }
+                catch (Exception ex)
+                {
+                    writer.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
