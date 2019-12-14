86135f27cb3e4b8ca50ab106a93a482ff8be93fe
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-12-14 22:21:53 +0200

    Add Tests

--- C# OOP/Exams/C# OOP Exam - 11 August 2019/Tests/BlueOrigin.Tests/SpaceshipTests.cs
+++ C# OOP/Exams/C# OOP Exam - 11 August 2019/Tests/BlueOrigin.Tests/SpaceshipTests.cs
@@ -20,5 +20,55 @@ namespace BlueOrigin.Tests
             Assert.Throws<ArgumentNullException>(() => new Spaceship(name, 30), "Invalid spaceship name!");
         }
 
+        [Test]
+        public void ThrowsExceptionWhenCreateSpaceshipWithLessThanZeroCapacity()
+        {
+            Assert.Throws<ArgumentException>(() => new Spaceship("Ivan", -1));
+        }
+
+        [Test]
+        public void ThrowsExceptionWhenAddNewAstronautWhenCapacityIsFull()
+        {
+            Spaceship spaceship = new Spaceship("Universe", 1);
+            spaceship.Add(new Astronaut("Rus", 45.5));
+
+            Assert.Throws<InvalidOperationException>(() => spaceship.Add(new Astronaut("Ivan", 33.3)), "Spaceship is full!");
+        }
+
+        [Test]
+        public void ThrowsExceptionWhenAddNewAstronautAndSameNameAlreadyExists()
+        {
+            Spaceship spaceship = new Spaceship("Universe", 5);
+            spaceship.Add(new Astronaut("Rus", 100));
+
+            Assert.Throws<InvalidOperationException>(() => spaceship.Add(new Astronaut("Rus", 44.5)), "Astronaut Rus is already in!");
+        }
+
+        //[Test]
+        //public void ReturnsCorrectCount()
+        //{
+        //    Spaceship spaceship = new Spaceship("Universe", 20);
+        //    spaceship.Add(new Astronaut("Ivan", 45.6));
+
+        //    Assert.That(spaceship.Count == 1);
+        //}
+
+        [Test]
+        public void ReturnsFalseWhenRemoveData()
+        {
+            Spaceship spaceship = new Spaceship("Universe", 12);
+            spaceship.Add(new Astronaut("Rus", 76));
+
+            Assert.False(spaceship.Remove("Ivan"));
+        }
+
+        [Test]
+        public void ReturnsTrueWhenRemoveData()
+        {
+            Spaceship spaceship = new Spaceship("Universe", 12);
+            spaceship.Add(new Astronaut("Rus", 76));
+
+            Assert.True(spaceship.Remove("Rus"));
+        }
     }
 }
