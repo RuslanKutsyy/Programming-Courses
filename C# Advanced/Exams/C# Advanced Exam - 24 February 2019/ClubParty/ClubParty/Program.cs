3f241c9354bf44f6cfdc0010614b3ce3b9535eb7
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2019-12-15 19:35:12 +0200

    ClubParty Unfinished

---
+++ C# Advanced/Exams/C# Advanced Exam - 24 February 2019/ClubParty/ClubParty/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClubParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hallMaxCapacity = int.Parse(Console.ReadLine());
+            Stack<string> reservationData = new Stack<string>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries));
+
+            Dictionary<string, int> reservations = new Dictionary<string, int>();
+
+            while (true)
+            {
+                if (reservationData.Count == 0)
+                {
+                    break;
+                }
+
+                var sign = reservationData.Pop();
+
+
+
+            }
+            
+        }
+    }
+}
