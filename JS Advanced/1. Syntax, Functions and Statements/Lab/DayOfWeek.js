9d022aafa7e7b573b63697a8a59a57124a568233
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-01-15 13:50:24 +0200

    Create DayOfWeek.js

---
+++ JS Advanced/1. Syntax, Functions and Statements/Lab/DayOfWeek.js
@@ -0,0 +1,21 @@
+function dayOfWeek(arg) {
+    if (arg === 'Monday') {
+        console.log(1);        
+    } else if (arg === 'Tuesday') {
+        console.log(2);        
+    } else if (arg === 'Wednesday') {
+        console.log(3);        
+    } else if (arg === 'Thursday') {
+        console.log(4);        
+    } else if (arg === 'Friday') {
+        console.log(5);        
+    } else if (arg === 'Saturday') {
+        console.log(6);        
+    } else if (arg === 'Sunday') {
+        console.log(7);        
+    } else {
+        console.log('error');        
+    }
+}
+
+dayOfWeek();
