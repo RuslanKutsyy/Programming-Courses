f82e27962ae61d107cc92981597cb9f1f5a79401
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-01-21 22:09:57 +0200

    Create 04. Last K Numbers Sequence.js

---
+++ JS Advanced/2. Arrays/Lab/04. Last K Numbers Sequence.js
@@ -0,0 +1,13 @@
+function numberSequence(n, k) {
+    
+    let result = [1];
+    for(let i=1; i<n; i++) {
+        let startIndex = Math.max(0, i-k);
+        let currentElement = result.slice(startIndex, startIndex + k).reduce((a, b) => a + b);
+        result.push(currentElement);
+    }
+
+    console.log(result.join(" "));
+}
+
+numberSequence(6, 3);
