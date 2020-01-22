09139ed95d6bda34a6b332fc39a5798e197146c7
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-01-22 16:54:33 +0200

    Create 05. Extract Increasing Subsequence from Array.js

---
+++ JS Advanced/2. Arrays/Exercises/05. Extract Increasing Subsequence from Array.js
@@ -0,0 +1,23 @@
+function extract(array) {
+    let newArr = [];
+    let max = Number.MIN_SAFE_INTEGER;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] >= max) {
+            max = array[i];
+            newArr.push(array[i]);
+        }
+    }
+
+    console.log(newArr.join('\n'));
+}
+
+extract([1, 
+    3, 
+    8, 
+    4, 
+    10, 
+    12, 
+    3, 
+    2, 
+    24]);
