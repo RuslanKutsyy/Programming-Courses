7f5e2876d1f07bd65a2b89c0f8654b4c3ea436d4
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-01-28 14:50:03 +0200

    Create adjacentElementsProduct.js

---
+++ CodeSignal Exercises/Edge of the Ocean/adjacentElementsProduct.js
@@ -0,0 +1,10 @@
+function adjacentElementsProduct(inputArray) {
+    let max = Number.MIN_SAFE_INTEGER;
+    for (let i = 0; i < inputArray.length; i++) {
+        let temp = inputArray[i] * inputArray[i+1];
+        if (temp > max) {
+            max = temp;
+        }
+        }
+    return max;
+}
