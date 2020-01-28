653fa7c8fa108d619fd038c72520082c5e21373d
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-01-28 18:43:08 +0200

    Create almostIncreasingSequence.js

---
+++ CodeSignal Exercises/Edge of the Ocean/almostIncreasingSequence.js
@@ -0,0 +1,11 @@
+function almostIncreasingSequence(sequence) {
+    let increasing = true;
+    for (let i = 0; i < sequence.length; i++){
+        if (sequence[ I + 1] <= sequence) {
+            increasing = false;
+            //sequence[i + 1]
+        }
+    }
+}
+
+almostIncreasingSequence([1, 3, 2, 1]);
