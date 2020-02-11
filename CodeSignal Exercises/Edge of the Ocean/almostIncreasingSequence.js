e375986a9c6d6286ff546b371b179cd6a97761d8
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-02-11 18:42:24 +0200

    Update almostIncreasingSequence.js

--- CodeSignal Exercises/Edge of the Ocean/almostIncreasingSequence.js
+++ CodeSignal Exercises/Edge of the Ocean/almostIncreasingSequence.js
@@ -1,11 +1,36 @@
 function almostIncreasingSequence(sequence) {
-    let increasing = true;
+    let increaseCheck;
     for (let i = 0; i < sequence.length; i++){
-        if (sequence[ I + 1] <= sequence) {
-            increasing = false;
-            //sequence[i + 1]
+        let tempArr = [];
+
+        for (let el of sequence) {
+            if (el != sequence[i]) {
+                tempArr.push(el);
+            }
         }
+
+        increaseCheck = increasing(tempArr);
+
+        if (increaseCheck === true) {
+            break;
+        }
+
+    }
+
+    console.log(increaseCheck);
+
+
+    function increasing(someArray) {
+        let current = someArray[0];
+        for (let i = 1; i < someArray.length; i++) {
+            if (someArray[i] <= current) {
+                return false;
+            }
+            current = someArray[i];
+        }
+
+        return true;
     }
 }
 
-almostIncreasingSequence([1, 3, 2, 1]);
+almostIncreasingSequence([1, 1, 1, 2, 3]);
