a8f2ab2ab469de34f70bf6a699d016eb9afe4091
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-02-12 23:01:36 +0200

    Update 04. Command Processor.js

--- JS Advanced/4. Advanced Functions/Lab/04. Command Processor.js
+++ JS Advanced/4. Advanced Functions/Lab/04. Command Processor.js
@@ -1,17 +1,17 @@
 function solution() {
     let str = '';
+
     return {
-        append: (s) => str += s,
+        append: (arg) => str += arg,
         removeStart: (n) => str = str.substring(n),
         removeEnd: (n) => str = str.substring(0, str.length - n),
         print: () => console.log(str)
     }
 }
 
-let secondZeroTest = solution();
+let firstZeroTest = solution();
 
-secondZeroTest.append('123');
-secondZeroTest.append('45');
-secondZeroTest.removeStart(2);
-secondZeroTest.removeEnd(1);
-secondZeroTest.print();
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
