c6627342ff23008a3eb86e16e510bd0baa27b491
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-03-28 22:40:21 +0200

    Update 01. Sort Array.js

--- JS Advanced/4. Advanced Functions/Exercises/01. Sort Array.js
+++ JS Advanced/4. Advanced Functions/Exercises/01. Sort Array.js
@@ -1,5 +1,5 @@
-function sortArray(arg1, arg2) {
-    return arg1.sort((a, b) => arg2 === 'asc' ? a - b : b - a);
+function sortArray(data, criteria) {
+    return criteria === 'asc' ? data.sort((a, b) => a - b) : data.sort((a, b) => b - a);
 }
 
-sortArray([14, 7, 17, 6, 8], 'desc');
+sortArray([14, 7, 17, 6, 8], 'asc');
