cdbf95c90d04252801d50201420a36c5bb658d36
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-03-28 20:03:03 +0200

    Update 03. Filter Employees.js

--- JS Advanced/4. Advanced Functions/Lab/03. Filter Employees.js
+++ JS Advanced/4. Advanced Functions/Lab/03. Filter Employees.js
@@ -1,39 +1,41 @@
-function filterEmployees(data, criteria) {
-    let employees = JSON.parse(data);
-    let criteriaData = criteria.split('-');
-    let criteriaName = criteriaData[0];
-    let criteriaValue = criteriaData[1];
-    let arr = Array.from(employees.filter(employee => employee[criteriaName] === criteriaValue));
+function filterEmployees(data, criteriaAsText) {
+  let employeeArr = JSON.parse(data);
+  let criteria = criteriaAsText.split('-')[0];
+  let filteredData = [];
+  if (criteria === 'all') {
+    filteredData = employeeArr;
+  } else {
+    let criteriaValue = criteriaAsText.split('-')[1];
+    filteredData = employeeArr.filter(obj => obj[criteria] === criteriaValue);
+  }
 
-    for (let i = 0; i < arr.length; i++) {
-        console.log(`${i}. ${arr[i].first_name} ${arr[i].last_name} - ${arr[i].email}`);
-    }
+  return filteredData.forEach(el => console.log(`${filteredData.indexOf(el)}. ${el.first_name} ${el.last_name} - ${el.email}`));
 }
 
 filterEmployees(`[{
-    "id": "1",
-    "first_name": "Kaylee",
-    "last_name": "Johnson",
-    "email": "k0@cnn.com",
-    "gender": "Female"
-  }, {
-    "id": "2",
-    "first_name": "Kizzee",
-    "last_name": "Johnson",
-    "email": "kjost1@forbes.com",
-    "gender": "Female"
-  }, {
-    "id": "3",
-    "first_name": "Evanne",
-    "last_name": "Maldin",
-    "email": "emaldin2@hostgator.com",
-    "gender": "Male"
-  }, {
-    "id": "4",
-    "first_name": "Evanne",
-    "last_name": "Johnson",
-    "email": "ev2@hostgator.com",
-    "gender": "Male"
-  }]`,
- 'last_name-Johnson'
+  "id": "1",
+  "first_name": "Kaylee",
+  "last_name": "Johnson",
+  "email": "k0@cnn.com",
+  "gender": "Female"
+}, {
+  "id": "2",
+  "first_name": "Kizzee",
+  "last_name": "Johnson",
+  "email": "kjost1@forbes.com",
+  "gender": "Female"
+}, {
+  "id": "3",
+  "first_name": "Evanne",
+  "last_name": "Maldin",
+  "email": "emaldin2@hostgator.com",
+  "gender": "Male"
+}, {
+  "id": "4",
+  "first_name": "Evanne",
+  "last_name": "Johnson",
+  "email": "ev2@hostgator.com",
+  "gender": "Male"
+}]`,
+'all'
 );
