54952b1a067399514add09e3961bddbf1ac77d6f
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-03-28 19:23:27 +0200

    Update 02. Currency Format.js

--- JS Advanced/4. Advanced Functions/Lab/02. Currency Format.js
+++ JS Advanced/4. Advanced Functions/Lab/02. Currency Format.js
@@ -1,12 +1,3 @@
-function returnData(currencyFormatter) {
-    let separator = ",";
-    let symbol = "$";
-    let symbolFirst = true;
-    
-    let dollarFormatter = value => currencyFormatter(separator, symbol, symbolFirst, value);
-    return dollarFormatter;
-}
-
 function currencyFormatter(separator, symbol, symbolFirst, value) {
     let result = Math.trunc(value) + separator;
     result += value.toFixed(2).substr(-2,2);
@@ -14,5 +5,15 @@ function currencyFormatter(separator, symbol, symbolFirst, value) {
     else return result + ' ' + symbol;
 }
 
-let dollarFormatter = returnData(currencyFormatter);
-console.log(dollarFormatter(5345));
+
+function result(func) {
+    let separator = ",";
+    let symbol = "$";
+    let symbolFirst = true;
+    return (input) => func(separator, symbol, symbolFirst, Number(input));
+}
+
+let dollarFormatter = result(currencyFormatter);
+console.log(dollarFormatter(5345));   // $ 5345,00
+console.log(dollarFormatter(3.1429)); // $ 3,14
+console.log(dollarFormatter(2.709));  // $ 2,71
