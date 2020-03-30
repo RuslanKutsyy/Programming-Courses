c6c6f82dfaa0e531c4e90bfa9fe86ca96fea3389
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-03-30 23:23:54 +0300

    Update 05. Breakfast Robot.js

--- JS Advanced/4. Advanced Functions/Exercises/05. Breakfast Robot.js
+++ JS Advanced/4. Advanced Functions/Exercises/05. Breakfast Robot.js
@@ -1,101 +1,71 @@
-let manager = (function solution() {
+let solution = (function() {
+    let microelements = {
+        carb : 0,
+        flavour : 0,
+        fat : 0,
+        protein : 0
+    };
 
-    const products = {
-        apple: {
-            carbohydrates: 1,
-            flavours: 2
+    let recipes = {
+        apple : {
+            carb : 1,
+            flavour : 2
         },
         lemonade : {
-            carbohydrates: 10,
-            flavours: 20
+            carb : 10,
+            flavour : 20
         },
         burger : {
-            carbohydrates: 5,
-            fat: 7,
-            flavours: 3
+            carb : 5,
+            fat : 7,
+            flavour : 3
         },
         eggs : {
-            protein: 5,
-            fat: 1,
-            flavours: 1
+            protein : 5,
+            fat : 1,
+            flavour : 1
         },
         turkey : {
-            protein: 10,
-            carbohydrates : 10,
-            fat: 10,
-            flavours: 10
+            protein : 10,
+            carb : 10,
+            fat : 10,
+            flavour : 10
         }
-    };
-
-    let storage = {
-        protein: 0,
-        carbohydrates: 0,
-        fat: 0,
-        flavours: 0
-    };
-
-    function addElementToStorage(type, quantity) {
-        storage[type]+=quantity;
-        return 'Success';
     }
 
-    function startCooking(type, quantity) {
-        let storageCheck = isEnoughElementstoCook();
+    function commandProcessing(command) {
+        let data = command.split(' ');
+        switch (data[0]) {
+            case 'restock': {
+                let microel = data[1];
+                let quantity = Number(data[2]);
+                if (microelements[microel] !== 'undefined') {
+                    microelements[microel] += quantity;
+                    return 'Success';
+                }
+                return `Error: not enough ${microel} in stock`;
+            };
+            case 'prepare': {
+                let meal = data[1];
+                let quantity = Number(data[2]);
+                if (recipes[meal] !== 'undefined') {
 
-        if (storageCheck === 'Success') {
-            cook();
-        }
-
-        return storageCheck;
-
-        function cook(){
-            for (let el of products[type]) {
-                let neededEl = products[type][el] * quantity;
-                storage[el] -= neededEl;
-            }
-        }
-
-        function isEnoughElementstoCook() {
-            let result = 'Success';
-
-            for (let el of products[type]) {
-                let needed = products[type][el] * quantity;
-
-                if (needed > storage[el]) {
-                    result = `Error: not enough ${el} in stock`;
-                    break;
                 }
             }
-            
-            return result;
         }
     }
 
-    function printReport() {
-        return object.keys(storage).map(key => {
-            return `${key}=${storage[key]}`;
-        }).join(' ');
+    function preparationCheck(meal){
+        
     }
 
-    return function (data) {
-        let [command, type, quantity] = data.split(' ');
-        quantity = Number(quantity);
-
-        const functions = {
-            restock: () => {
-                return addElementToStorage(type, quantity);
-            },
-            prepare: () => {
-                return startCooking(type, quantity);
-            },
-            report: () => {
-                return printReport();
-            }
-        };
-
-        return functions[command];
+    return function (input) {
+        commandProcessing(input);
     }
+
+    
 });
 
+let manager = solution();
 manager("restock flavour 50");  // Success
-manager("prepare lemonade 4");
+manager("prepare lemonade 4");  // Error: not enough carbohydrate in stock
