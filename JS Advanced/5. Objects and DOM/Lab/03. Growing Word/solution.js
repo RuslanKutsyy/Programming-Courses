6fc97b05195f07d13bfd74face4d779c95778fc0
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-04-17 01:15:29 +0300

    Add 'Growing Words ' Solution

---
+++ JS Advanced/5. Objects and DOM/Lab/03. Growing Word/solution.js
@@ -0,0 +1,13 @@
+function growingWord() {
+  let text = document.querySelectorAll('p')[2];
+
+  if (text.style.color === '' && text.style.fontSize === '') {
+    text.style.color = 'blue'; text.style.fontSize = 2 + 'px';
+  } else if (text.style.color === 'blue'){
+    text.style.color = 'green'; text.style.fontSize = parseInt(text.style.fontSize) * 2 + 'px';
+  } else if (text.style.color === 'green'){
+    text.style.color = 'red'; text.style.fontSize = parseInt(text.style.fontSize) * 2 + 'px';
+  } else if (text.style.color === 'red'){
+    text.style.color = 'blue'; text.style.fontSize = parseInt(text.style.fontSize) * 2 + 'px';
+  }
+}
