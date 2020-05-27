15fd5945a61bb72062b2428a2255d449b0d8764f
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-05-27 11:13:27 +0300

    Add "Single Inheritance" solution

---
+++ Java Course/Java OOP/Inheritance/Lab/Single Inheritance/src/com/company/Dog.java
@@ -0,0 +1,8 @@
+package com.company;
+import com.company.Animal;
+
+public class Dog extends Animal {
+    public void bark(){
+        System.out.println("barking...");
+    }
+}
---
+++ Java Course/Java OOP/Inheritance/Lab/Single Inheritance/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+import com.company.Animal;
+import com.company.Dog;
+public class Main {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}
