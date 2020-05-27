cd2541f1b23ea59f2a34d4ee1bf9b4a974cd7d39
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-05-28 00:04:08 +0300

    Add "Hierarchical_Inheritance" solution

---
+++ Java Course/Java OOP/Inheritance/Lab/Hierarchical_Inheritance/src/Main.java
@@ -0,0 +1,14 @@
+import Hierarchical_Inheritance.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+
+        Cat cat = new Cat();
+        cat.eat();
+        cat.meow();
+    }
+}
