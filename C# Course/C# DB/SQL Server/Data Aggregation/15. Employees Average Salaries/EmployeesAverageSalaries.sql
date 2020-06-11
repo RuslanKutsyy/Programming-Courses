5f74285b8b5f395712a282054aca4dd9e051349d
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-06-12 00:56:20 +0300

    Create EmployeesAverageSalaries.sql

---
+++ C# Course/C# DB/SQL Server/Data Aggregation/15. Employees Average Salaries/EmployeesAverageSalaries.sql
@@ -0,0 +1,14 @@
+SELECT * INTO EmployeesAverageSalaries
+FROM Employees AS e
+WHERE e.Salary > 30000
+
+DELETE FROM EmployeesAverageSalaries
+WHERE ManagerID = 42
+
+UPDATE EmployeesAverageSalaries
+SET Salary += 5000
+WHERE DepartmentID = 1
+
+SELECT eas.DepartmentID, AVG(eas.Salary) AS AverageSalary
+FROM EmployeesAverageSalaries AS eas
+GROUP BY eas.DepartmentID
