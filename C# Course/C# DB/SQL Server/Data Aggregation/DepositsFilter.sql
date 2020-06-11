f9b88791cc12c35463b572f9cfe9bfae138cb71a
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-06-11 20:53:38 +0300

    Update Structure

--- C# Course/C# DB/SQL Server/Data Aggregation/DepositsFilter.sql
+++
@@ -1,7 +0,0 @@
-SELECT a.DepositGroup, SUM(a.DepositAmount) AS TotalSum
-FROM (SELECT wd.DepositGroup, wd.DepositAmount, wd.MagicWandCreator
-FROM WizzardDeposits AS wd) AS a
-WHERE a.MagicWandCreator LIKE 'Ollivander family'
-GROUP BY a.DepositGroup
-HAVING SUM(a.DepositAmount) < 150000
-ORDER BY TotalSum DESC
