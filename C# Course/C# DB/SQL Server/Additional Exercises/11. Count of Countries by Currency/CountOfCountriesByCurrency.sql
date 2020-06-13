6313096bbd116028346607b950d4ad759447eaf9
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-06-13 22:16:48 +0300

    Create CountOfCountriesByCurrency.sql

---
+++ C# Course/C# DB/SQL Server/Additional Exercises/11. Count of Countries by Currency/CountOfCountriesByCurrency.sql
@@ -0,0 +1,6 @@
+SELECT curr.CurrencyCode, curr.Description AS [Currency], COUNT(c.CountryCode) AS [NumberOfCountries]
+FROM Currencies AS curr
+LEFT JOIN Countries AS c
+	ON curr.CurrencyCode = c.CurrencyCode
+GROUP BY curr.CurrencyCode, curr.Description
+ORDER BY [NumberOfCountries] DESC, [Currency]
