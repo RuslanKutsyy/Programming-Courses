77bc12ff3dcd99de9bf23f831b101c03fdd93cb3
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-06-14 14:28:56 +0300

    Add 02. Employees with Salary Above Number Solution

--- C# Course/C# DB/SQL Server/Database Programmability and Transactions/DatabaseProgrammabilityAndTransactions.sql
+++ C# Course/C# DB/SQL Server/Database Programmability and Transactions/DatabaseProgrammabilityAndTransactions.sql
@@ -4,5 +4,17 @@ CREATE PROCEDURE usp_GetEmployeesSalaryAbove35000
 AS (SELECT e.FirstName, e.LastName
 FROM Employees AS e
 WHERE e.Salary > 35000)
+GO
 
 EXEC usp_GetEmployeesSalaryAbove35000
+GO
+
+-- Problem 2 - Employees with Salary Above Number
+
+CREATE PROCEDURE usp_GetEmployeesSalaryAboveNumber(@SalaryLevel DECIMAL(18,4))
+AS
+	SELECT e.FirstName, e.LastName
+	FROM Employees AS e
+	WHERE e.Salary >= @SalaryLevel
+
+GO
