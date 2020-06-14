696ed1dc8abebe5f992fb399b68a0c334a688073
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-06-14 21:11:30 +0300

    Add "10. People with Balance Higher Than" Solution

--- C# Course/C# DB/SQL Server/Database Programmability and Transactions/DatabaseProgrammabilityAndTransactions_part2.sql
+++ C# Course/C# DB/SQL Server/Database Programmability and Transactions/DatabaseProgrammabilityAndTransactions_part2.sql
@@ -6,3 +6,17 @@ AS
 	FROM AccountHolders AS ah
 
 GO
+
+-- Problem 10 - People with Balance Higher Than
+
+CREATE PROCEDURE usp_GetHoldersWithBalanceHigherThan (@balanceLevel DECIMAL(16,2))
+AS
+	SELECT ah.FirstName AS [First Name], ah.LastName AS [Last Name]
+	FROM AccountHolders AS ah
+	JOIN Accounts AS a
+		ON ah.Id = a.AccountHolderId
+	GROUP BY ah.FirstName, ah.LastName
+	HAVING SUM(a.Balance) > @balanceLevel
+	ORDER BY ah.FirstName, ah.LastName
+
+GO
