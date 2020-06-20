0c5fc921828ddd3c3d6ea11a2b51696dac9dde23
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-06-20 22:37:50 +0300

    Add "9. Full Info" solution

--- C# Course/C# DB/SQL Server/Exams/Exam Preparation 1/prep_exam1_solution.sql
+++ C# Course/C# DB/SQL Server/Exams/Exam Preparation 1/prep_exam1_solution.sql
@@ -121,3 +121,24 @@ FROM Passengers as p
 WHERE p.Id NOT IN
 (SELECT t.PassengerId FROM Tickets AS t)
 ORDER BY P.Age DESC, p.FirstName, p.LastName
+
+
+--09. Full Info
+
+SELECT  p.FirstName + ' ' + p.LastName AS [Full Name],
+		pl.Name AS [Plane Name],
+		f.Origin + ' - ' + f.Destination AS [Trip],
+		lt.Type AS [Luggage Type]
+FROM Tickets AS t
+JOIN Passengers AS p
+	ON t.PassengerId = p.Id
+JOIN Flights AS f
+	ON t.FlightId = f.Id
+JOIN Planes AS pl
+	ON f.PlaneId = pl.Id
+JOIN Luggages AS l
+	ON t.LuggageId = l.Id
+JOIN LuggageTypes AS lt
+	ON l.LuggageTypeId = lt.Id
+ORDER BY [Full Name], [Plane Name],
+		 f.Origin, f.Destination, [Luggage Type]
