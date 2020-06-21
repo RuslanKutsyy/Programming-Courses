0ea9fab5ae2adc21ada56e13e46650c447a7f652
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-06-21 09:39:36 +0300

    Create exam_solution.sql

---
+++ C# Course/C# DB/SQL Server/Exams/exam_solution.sql
@@ -0,0 +1,75 @@
+--01. DDL
+
+CREATE TABLE Cities
+(
+	Id INT NOT NULL PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(20) NOT NULL,
+	CountryCode VARCHAR(2) NOT NULL
+)
+
+CREATE TABLE Hotels
+(
+	Id INT NOT NULL PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(30) NOT NULL,
+	CityId INT NOT NULL FOREIGN KEY REFERENCES Cities(Id),
+	EmployeeCount INT NOT NULL,
+	BaseRate MONEY
+)
+
+CREATE TABLE Rooms
+(
+	Id INT NOT NULL PRIMARY KEY IDENTITY,
+	Price MONEY NOT NULL,
+	[Type] NVARCHAR(20) NOT NULL,
+	Beds INT NOT NULL,
+	HotelId INT NOT NULL FOREIGN KEY REFERENCES Hotels(Id)
+)
+
+CREATE TABLE Trips
+(
+	Id INT NOT NULL PRIMARY KEY IDENTITY,
+	RoomId INT NOT NULL FOREIGN KEY REFERENCES Rooms(Id),
+	BookDate DATE NOT NULL,
+	ArrivalDate DATE NOT NULL,
+	ReturnDate DATE NOT NULL,
+	CancelDate DATE
+	CONSTRAINT CH_BookDate CHECK(ArrivalDate > BookDate),
+	CONSTRAINT CH_ArrivalDate CHECK(ReturnDate > ArrivalDate)
+)
+
+CREATE TABLE Accounts
+(
+	Id INT NOT NULL PRIMARY KEY IDENTITY,
+	FirstName NVARCHAR(50) NOT NULL,
+	MiddleName NVARCHAR(20),
+	LastName NVARCHAR(50) NOT NULL,
+	CityId INT NOT NULL FOREIGN KEY REFERENCES Cities(Id),
+	BirthDate DATE NOT NULL,
+	Email VARCHAR(100) NOT NULL UNIQUE
+)
+
+CREATE TABLE AccountsTrips
+(
+	AccountId INT NOT NULL FOREIGN KEY REFERENCES Accounts(Id),
+	TripId INT NOT NULL FOREIGN KEY REFERENCES Trips(Id),
+	Luggage INT NOT NULL DEFAULT 0
+	CONSTRAINT PK_AccountTrips PRIMARY KEY (AccountId, TripId)
+)
+
+
+--02. Insert
+
+INSERT INTO Accounts (FirstName, MiddleName, LastName, CityId, BirthDate, Email)
+VALUES
+('John', 'Smith', 'Smith', 34, '1975-07-21', 'j_smith@gmail.com'),
+('Gosho', NULL, 'Petrov', 11, '1978-05-16',	'g_petrov@gmail.com'),
+('Ivan', 'Petrovich', 'Pavlov', 59, '1849-09-26', 'i_pavlov@softuni.bg'),
+('Friedrich', 'Wilhelm', 'Nietzsche', 2, '1844-10-15', 'f_nietzsche@softuni.bg')
+
+INSERT INTO Trips (RoomId, BookDate, ArrivalDate, ReturnDate, CancelDate)
+VALUES
+(101, '2015-04-12', '2015-04-14', '2015-04-20',	'2015-02-02'),
+(102, '2015-07-07',	'2015-07-15', '2015-07-22',	'2015-04-29'),
+(103, '2013-07-17',	'2013-07-23', '2013-07-24',	NULL),
+(104, '2012-03-17',	'2012-03-31', '2012-04-01',	2012-01-10),
+(109, '2017-08-07',	'2017-08-28', '2017-08-29',	NULL)
