42113368a8ff08dac917183df605a609ae9b05df
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-06-22 23:30:56 +0300

    Add "ADO.Net" exercise solutions

---
+++ C# Course/C# DB/Entity Framework Core/ADO.NET/ADO.NET Exercise/ADO.NET Exercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ADO.NET_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=SQLSRV;Database=MinionsDB;User ID = RemoteAdmin; Password=*******";
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+
+            using (sqlConnection)
+            {
+                string sqlCommandText = "SELECT CONCAT(v.Name,' - ', COUNT(mv.MinionId)) FROM Villains AS v " +
+                    "JOIN MinionsVillains AS mv ON mv.VillainId = v.Id GROUP BY v.Name, v.EvilnessFactorId";
+                SqlCommand command = new SqlCommand(sqlCommandText, sqlConnection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0]);
+                }
+            }
+        }
+    }
+}
