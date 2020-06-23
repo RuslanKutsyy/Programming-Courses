2b2c973cc7c57afec2cffad750473eb353d972b5
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-06-24 01:13:49 +0300

    Add "Minion_Names" solution

---
+++ C# Course/C# DB/Entity Framework Core/ADO.NET/ADO.NET Exercise/Minion_Names/ErrorMessages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minion_Names
+{
+    public static class ErrorMessages
+    {
+        public static string noSuchVillain = "No villain with ID {id} exists in the database.";
+    }
+}
--- C# Course/C# DB/Entity Framework Core/ADO.NET/ADO.NET Exercise/Minion_Names/Program.cs
+++ C# Course/C# DB/Entity Framework Core/ADO.NET/ADO.NET Exercise/Minion_Names/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Data.SqlClient;
+using System.Net;
+using System.Security;
+using System.Linq;
 
 namespace Minion_Names
 {
@@ -6,7 +10,46 @@ namespace Minion_Names
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int id = int.Parse(Console.ReadLine());
+            string connectionString = getConnectionString("SQLSRV", "MinionsDB");
+            SqlConnection dbConn = new SqlConnection(connectionString);
+            dbConn.Open();
+
+            using (dbConn)
+            {
+                string queryText = $"SELECT 'Villain: ' + Name FROM Villains WHERE Id = {id}";
+                SqlCommand sqlCmd = new SqlCommand(queryText, dbConn);
+                string villainData = (string)sqlCmd.ExecuteScalar();
+
+                if (villainData == null)
+                {
+                    //Console.WriteLine(String.Format(ErrorMessages.noSuchVillain, id.ToString()));
+                    Console.WriteLine($"No villain with ID {id} exists in the database.");
+                    return;
+                }
+
+                Console.WriteLine(villainData);
+
+                queryText = "SELECT m.Name, m.Age FROM MinionsVillains AS mv" +
+                            "JOIN Minions AS m ON mv.MinionId = m.Id" +
+                            $"WHERE mv.VillainId = {id}";
+
+                SqlDataReader reader = sqlCmd.ExecuteReader();
+
+
+                int counter = 1;
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{counter}. {reader[0]}");
+                    counter++;
+                }
+            }
+        }
+
+        public static string getConnectionString(string serverName, string database)
+        {
+            return $"Server={serverName};Database={database};User ID = RemoteAdmin;Password = P@ssword1";
         }
     }
 }
