f217bb1f1921007485aa3ef1edd2c15de649b982
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-07-02 00:42:44 +0300

    Add "Employees_with_Salary_Over_50000" solution

---
+++ C# Course/C# DB/Entity Framework Core/EXERCISE_ENTITY_FRAMEWORK_INTRODUCTION/Employees_with_Salary_Over_50000/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(dbContext));
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+            context.Employees.Where(x => x.Salary > 50000).OrderBy(x => x.FirstName).Select(x => sb.AppendLine($"{x.FirstName} - {x.Salary:F2}")).ToList();
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
