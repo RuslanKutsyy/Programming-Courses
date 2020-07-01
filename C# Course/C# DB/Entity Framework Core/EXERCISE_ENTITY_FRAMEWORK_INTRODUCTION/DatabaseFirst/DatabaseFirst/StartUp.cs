2323a261108bf0532dd3f452ee5287a9c0c84f14
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-07-02 00:01:07 +0300

    Add "03. Employees Full Information" solution

---
+++ C# Course/C# DB/Entity Framework Core/EXERCISE_ENTITY_FRAMEWORK_INTRODUCTION/DatabaseFirst/DatabaseFirst/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Primitives;
+using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var dbData = new SoftUniContext();
+            Console.WriteLine(GetEmployeesFullInformation(dbData));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+            context.Employees.OrderBy(x => x.EmployeeId)
+                .Select(x => sb.AppendLine($"{x.FirstName} {x.LastName} {x.MiddleName} {x.JobTitle} {x.Salary:F2}")).ToArray();
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
