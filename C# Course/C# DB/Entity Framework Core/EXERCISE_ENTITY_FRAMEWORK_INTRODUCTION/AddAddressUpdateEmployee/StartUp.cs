9da13f3a75140c367eb648c0fd832e3e4693c218
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-07-02 23:02:01 +0300

    Add "06. Adding a New Address and Updating Employee" Solution

---
+++ C# Course/C# DB/Entity Framework Core/EXERCISE_ENTITY_FRAMEWORK_INTRODUCTION/AddAddressUpdateEmployee/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+            Console.WriteLine(AddNewAddressToEmployee(dbContext));
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            var address = new Address();
+            address.AddressText = "Vitoshka 15";
+            address.TownId = 4; ;
+            context.Addresses.Add(address);
+
+            var worker = context.Employees.Where(x => x.LastName.Equals("Nakov")).FirstOrDefault();
+            worker.Address = address;
+            address.Employees.Add(worker);
+            context.SaveChanges();
+
+            var sb = new StringBuilder();
+
+            context.Employees.OrderByDescending(x => x.AddressId).Take(10).Select(x => sb.AppendLine($"{x.Address.AddressText}")).ToList();
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
