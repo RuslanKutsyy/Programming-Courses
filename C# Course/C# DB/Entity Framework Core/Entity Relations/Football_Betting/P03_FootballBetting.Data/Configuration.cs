14ee1510c7f2907f713c1020cd6ad01c2a3ccdc4
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-07-09 19:13:08 +0300

    Add "Football Betting" Solution

---
+++ C# Course/C# DB/Entity Framework Core/Entity Relations/Football_Betting/P03_FootballBetting.Data/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString =
+            "Server=SQLSRV;Database=FootballBetting;User ID=RemoteUser;Password=P@ssword1;";
+    }
+}
---
+++ C# Course/C# DB/Entity Framework Core/Entity Relations/Football_Betting/P03_FootballBetting.Data/FootballBettingContext.cs
@@ -0,0 +1,193 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_FootballBetting.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace P03_FootballBetting.Data
+{
+    public class FootballBettingContext : DbContext
+    {
+        public FootballBettingContext()
+        {
+        }
+
+        public FootballBettingContext(DbContextOptions<FootballBettingContext> options)
+            :base(options)
+        {
+        }
+
+        public virtual DbSet<Team> Teams { get; set; }
+        public virtual DbSet<Color> Colors { get; set; }
+        public virtual DbSet<Town> Towns { get; set; }
+        public virtual DbSet<Country> Countries { get; set; }
+        public virtual DbSet<Player> Players { get; set; }
+        public virtual DbSet<Position> Positions { get; set; }
+        public virtual DbSet<PlayerStatistic> PlayerStatistics { get; set; }
+        public virtual DbSet<Game> Games { get; set; }
+        public virtual DbSet<Bet> Bets { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Team>(entity =>
+            {
+                entity.HasKey(t => t.TeamId);
+
+                entity.Property(t => t.Name)
+                .HasColumnType("NVARCHAR(100)")
+                .IsRequired()
+                .IsUnicode();
+
+                entity.Property(t => t.Initials)
+                .HasColumnType("NVARCHAR(3)")
+                .HasMaxLength(3)
+                .IsRequired();
+
+                entity.Property(t => t.Budget)
+                .IsRequired();
+
+                entity.HasOne(t => t.PrimaryKitColor)
+                .WithMany(pkc => pkc.PrimaryKitTeams)
+                .HasForeignKey(t => t.PrimaryKitColorId);
+
+                entity.HasOne(t => t.SecondaryKitColor)
+                .WithMany(skc => skc.SecondaryKitTeams)
+                .HasForeignKey(t => t.SecondaryKitColorId);
+
+                entity.HasOne(t => t.Town)
+                .WithMany(to => to.Teams)
+                .HasForeignKey(t => t.TownId);
+            });
+
+            modelBuilder.Entity<Color>(entity =>
+            {
+                entity.HasKey(c => c.ColorId);
+
+                entity.Property(c => c.Name)
+                .HasColumnType("NVARCHAR(50)")
+                .IsRequired();
+
+                entity.HasMany(c => c.PrimaryKitTeams);
+
+                entity.HasMany(c => c.SecondaryKitTeams);
+            });
+
+            modelBuilder.Entity<Town>(entity =>
+            {
+                entity.HasKey(t => t.TownId);
+
+                entity.Property(t => t.Name)
+                .HasColumnType("NVARCHAR(100)")
+                .IsRequired();
+
+                entity.HasOne(t => t.Country)
+                .WithMany(c => c.Towns)
+                .HasForeignKey(t => t.CountryId);
+            });
+
+            modelBuilder.Entity<Country>(entity =>
+            {
+                entity.HasKey(c => c.CountryId);
+
+                entity.Property(c => c.Name)
+                .HasColumnType("NVARCHAR(100)")
+                .IsRequired();
+            });
+
+            modelBuilder.Entity<Player>(entity =>
+            {
+                entity.HasKey(p => p.PlayerId);
+
+                entity.Property(p => p.Name)
+                .HasColumnType("NVARCHAR(100)")
+                .IsRequired();
+
+                entity.Property(p => p.SquadNumber)
+                .IsRequired();
+
+                entity.HasOne(p => p.Team)
+                .WithMany(t => t.Players)
+                .HasForeignKey(p => p.TeamId);
+
+                entity.HasOne(p => p.Position)
+                .WithMany(pos => pos.Players)
+                .HasForeignKey(p => p.PositionId);
+
+                entity.HasMany(p => p.PlayerStatistics);
+            });
+
+            modelBuilder.Entity<Position>(entity =>
+            {
+                entity.HasKey(p => p.PositionId);
+
+                entity.Property(p => p.Name)
+                .HasColumnType("NVARCHAR(50)")
+                .IsRequired();
+            });
+
+            modelBuilder.Entity<Game>(entity =>
+            {
+                entity.HasKey(g => g.GameId);
+
+                entity.HasOne(g => g.HomeTeam)
+                .WithMany(t => t.HomeGames)
+                .HasForeignKey(g => g.HomeTeamId);
+
+                entity.HasOne(g => g.AwayTeam)
+                .WithMany(t => t.AwayGames)
+                .HasForeignKey(g => g.AwayTeamId);
+
+                entity.HasMany(g => g.PlayerStatistics);
+
+                entity.HasMany(g => g.Bets);
+            });
+
+            modelBuilder.Entity<PlayerStatistic>(entity =>
+            {
+                entity.HasKey(ps => new { ps.GameId, ps.PlayerId });
+
+                entity.Property(ps => ps.GameId).IsRequired();
+
+                entity.Property(ps => ps.PlayerId).IsRequired();
+
+                entity.HasOne(ps => ps.Player)
+                .WithMany(p => p.PlayerStatistics)
+                .HasForeignKey(ps => ps.PlayerId);
+
+                entity.HasOne(ps => ps.Game)
+                .WithMany(g => g.PlayerStatistics)
+                .HasForeignKey(ps => ps.GameId);
+            });
+
+            modelBuilder.Entity<Bet>(entity =>
+            {
+                entity.HasKey(b => b.BetId);
+
+                entity.Property(b => b.Amount).IsRequired();
+
+                entity.Property(b => b.Prediction).IsRequired();
+
+                entity.HasOne(b => b.Game);
+
+                entity.Property(b => b.Prediction)
+                .IsRequired();
+
+                entity.HasOne(b => b.User)
+                .WithMany(u => u.Bets);
+            });
+        }
+    }
+}
