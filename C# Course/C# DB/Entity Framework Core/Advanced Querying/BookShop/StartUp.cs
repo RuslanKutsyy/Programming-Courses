51aee8f002a9b9ac034abddac82e76317bd7dfe5
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-07-15 10:58:45 +0300

    Add "GetAuthorNamesEndingIn" method

--- C# Course/C# DB/Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
+++ C# Course/C# DB/Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
@@ -19,8 +19,8 @@
             using var db = new BookShopContext();
             //DbInitializer.ResetDatabase(db);
 
-            var date = Console.ReadLine();
-            Console.WriteLine(GetBooksReleasedBefore(db, date));
+            var input = Console.ReadLine();
+            Console.WriteLine(GetAuthorNamesEndingIn(db, input));
         }
 
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
@@ -114,6 +114,21 @@
                     x.Price
                 }).ToList().ForEach(x => sb.AppendLine($"{x.Title} - {x.EditionType} - ${x.Price:F2}"));
 
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetAuthorNamesEndingIn(BookShopContext context, string input)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            context.Authors.Where(x => x.FirstName.EndsWith(input))
+                .Select(x => new
+                {
+                    FullName = x.FirstName + " " + x.LastName,
+                })
+                .OrderBy(x => x.FullName)
+                .ToList().ForEach(x => sb.AppendLine(x.FullName));
 
             return sb.ToString().TrimEnd();
         }
