75e8157c66faf2fabc7937ac6015095a9d39e059
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-07-16 23:00:54 +0300

    Create circle.js

---
+++ JS Course/JS Advanced/7. Classes/Lab/circle.js
@@ -0,0 +1,12 @@
+class Circle{
+    constructor(radius){
+        this.radius = radius;
+    }
+
+    get diameter(){ return this.radius * 2};
+    set diameter(diameter){this.radius = diameter/2};
+
+    get area(){
+        return Math.PI * this.radius * this.radius;
+    }
+}
