908e6a90a8775124cb2a32dfd3a50bb2dfaf3c3e
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-07-22 00:59:12 +0300

    Add solution without Item and Order Controllers

---
+++ C# Course/C# DB/Entity Framework Core/C# AUTO MAPPING OBJECTS/FastFood.Core/ViewModels/Items/CreateItemInputModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FastFood.Core.ViewModels.Items
+{
+    public class CreateItemInputModel
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
