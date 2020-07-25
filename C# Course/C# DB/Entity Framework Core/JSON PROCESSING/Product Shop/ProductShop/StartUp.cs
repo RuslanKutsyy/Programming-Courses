8ac5a2f646690197f1b5936bf3f14b18f49ea45d
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-07-25 13:20:20 +0300

    Add "ImportProducts" method

--- C# Course/C# DB/Entity Framework Core/JSON PROCESSING/Product Shop/ProductShop/StartUp.cs
+++ C# Course/C# DB/Entity Framework Core/JSON PROCESSING/Product Shop/ProductShop/StartUp.cs
@@ -13,14 +13,24 @@ namespace ProductShop
         public static void Main(string[] args)
         {
             var dbContext = new ProductShopContext();
-            Console.WriteLine(ImportUsers(dbContext, File.ReadAllText(@"C:\\Users\rusla\\Documents\\GitHub\\SoftUniCourses\\C# Course\\C# DB\\Entity Framework Core\\JSON PROCESSING\\Product Shop\\ProductShop\\Datasets\\users.json")));
+            Console.WriteLine(ImportProducts(dbContext, File.ReadAllText(@"C:\\Users\rusla\\Documents\\GitHub\\SoftUniCourses\\C# Course\\C# DB\\Entity Framework Core\\JSON PROCESSING\\Product Shop\\ProductShop\\Datasets\\products.json")));
         }
 
         public static string ImportUsers(ProductShopContext context, string inputJson)
         {
             var jsonData = JsonConvert.DeserializeObject<List<User>>(inputJson);
 
-            context.AddRange(jsonData);
+            context.Users.AddRange(jsonData);
+            context.SaveChanges();
+
+            return $"Successfully imported {jsonData.Count}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            var jsonData = JsonConvert.DeserializeObject<List<Product>>(inputJson);
+
+            context.Products.AddRange(jsonData);
             context.SaveChanges();
 
             return $"Successfully imported {jsonData.Count}";
