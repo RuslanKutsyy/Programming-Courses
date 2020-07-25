9097438e345a59c5584f52076f35905b3be3c649
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-07-25 14:15:27 +0300

    Update "ImportCategories" method

--- C# Course/C# DB/Entity Framework Core/JSON PROCESSING/Product Shop/ProductShop/StartUp.cs
+++ C# Course/C# DB/Entity Framework Core/JSON PROCESSING/Product Shop/ProductShop/StartUp.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Castle.Core.Internal;
 using Newtonsoft.Json;
 using ProductShop.Data;
 using ProductShop.Models;
@@ -13,7 +14,7 @@ namespace ProductShop
         public static void Main(string[] args)
         {
             var dbContext = new ProductShopContext();
-            Console.WriteLine(ImportProducts(dbContext, File.ReadAllText(@"C:\\Users\rusla\\Documents\\GitHub\\SoftUniCourses\\C# Course\\C# DB\\Entity Framework Core\\JSON PROCESSING\\Product Shop\\ProductShop\\Datasets\\products.json")));
+            Console.WriteLine(ImportCategories(dbContext, File.ReadAllText(@"C:\\Users\rusla\\Documents\\GitHub\\SoftUniCourses\\C# Course\\C# DB\\Entity Framework Core\\JSON PROCESSING\\Product Shop\\ProductShop\\Datasets\\categories.json")));
         }
 
         public static string ImportUsers(ProductShopContext context, string inputJson)
@@ -35,5 +36,23 @@ namespace ProductShop
 
             return $"Successfully imported {jsonData.Count}";
         }
+
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            var jsonData = JsonConvert.DeserializeObject<List<Category>>(inputJson).Where(x => x.Name != null);
+            context.Categories.AddRange(jsonData);
+            context.SaveChanges();
+
+            return $"Successfully imported {jsonData.Count()}";
+        }
+
+        //public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
+        //{
+        //    var jsonData = JsonConvert.DeserializeObject<List<CategoryProduct>>(inputJson);
+        //    context.CategoryProducts.AddRange(jsonData);
+        //    context.SaveChanges();
+
+        //    return $"Successfully imported {jsonData.Count}";
+        //}
     }
 }
