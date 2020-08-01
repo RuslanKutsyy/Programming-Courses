44b12043a2fcbdf3097b38925d365b791379fe81
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-08-02 01:52:19 +0300

    Setup routes

--- MyAngularApp/src/app/app.module.ts
+++ MyAngularApp/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule} from '@angular/router';
+
+import { appRoutes} from '../routes';
+
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SectionSalesComponent } from './Sections/section-sales/section-sales.component';
+import { SectionOrdersComponent } from './Sections/section-orders/section-orders.component';
+import { SectionHealthComponent } from './Sections/section-health/section-health.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    SectionSalesComponent,
+    SectionOrdersComponent,
+    SectionHealthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
