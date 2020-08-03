66eee8e723c9574d5da99a27832994044bfae279
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-08-03 23:57:04 +0300

    Add solution and Import users

---
+++ C# Course/C# DB/Entity Framework Core/XML PROCESSING/ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using ProductShop.Data;
+using ProductShop.Dtos.Import;
+using ProductShop.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var usersXml = File.ReadAllText("../../../Datasets/users.xml");
+            var context = new ProductShopContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            var result = ImportUsers(context, usersXml);
+            Console.WriteLine(result);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            const string rootElement = "Users";
+            var usersResult = XmlConverter.Deserializer<ImportUserDto>(inputXml, rootElement);
+
+            List<User> users = new List<User>();
+
+            foreach (var importUserDto in usersResult)
+            {
+                var user = new User
+                {
+                    FirstName = importUserDto.FirstName,
+                    LastName = importUserDto.LastName,
+                    Age = importUserDto.Age
+                };
+                users.Add(user);
+            }
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }
+    }
+}
