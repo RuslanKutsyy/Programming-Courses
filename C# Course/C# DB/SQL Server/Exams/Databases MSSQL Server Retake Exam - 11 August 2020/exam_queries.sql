df45c3efa515d8d65ab37803797ced91e363c393
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-08-11 10:17:20 +0300

    Add "06. Negative Feedback" task

--- C# Course/C# DB/SQL Server/Exams/Databases MSSQL Server Retake Exam - 11 August 2020/exam_queries.sql
+++ C# Course/C# DB/SQL Server/Exams/Databases MSSQL Server Retake Exam - 11 August 2020/exam_queries.sql
@@ -111,3 +111,13 @@ FROM Products as p
 ORDER BY p.Price DESC, p.Name
 
 GO;
+
+--06. Negative Feedback
+
+SELECT f.ProductId, f.Rate, f.Description, f.CustomerId, c.Age, c.Gender
+FROM Feedbacks AS f
+JOIN Customers AS c ON f.CustomerId = c.Id
+WHERE f.Rate < 5
+ORDER BY f.ProductId DESC, f.Rate
+
+GO;
