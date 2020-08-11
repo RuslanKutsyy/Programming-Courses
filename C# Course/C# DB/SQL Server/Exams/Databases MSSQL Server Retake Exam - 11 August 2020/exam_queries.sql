8a7f50b20427791b4ed78d7e5eb7d0d5eab642de
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-08-11 10:04:29 +0300

    Add "03. Update" task

--- C# Course/C# DB/SQL Server/Exams/Databases MSSQL Server Retake Exam - 11 August 2020/exam_queries.sql
+++ C# Course/C# DB/SQL Server/Exams/Databases MSSQL Server Retake Exam - 11 August 2020/exam_queries.sql
@@ -61,21 +61,38 @@ CREATE TABLE ProductsIngredients
 	CONSTRAINT PK_ProductIngredients PRIMARY KEY (ProductId, IngredientId)
 )
 
-GO
+GO;
 
 --02. Insert
 
-INSERT INTO Clients (Name, CountryId, AddressText, Summary) VALUES
-('Teri', 'Ennaco', '570-889-5187'),
-('Merlyn', 'Lawler', '201-588-7810'),
-('Georgene', 'Montezuma', '925-615-5185'),
-('Jettie', 'Mconnell', '908-802-3564'),
-('Lemuel', 'Latzke', '631-748-6479'),
-('Melodie', 'Knipp', '805-690-1682'),
-('Candida', 'Corbley', '908-275-8357')
+INSERT INTO Distributors([Name], CountryId, AddressText, Summary) VALUES
+('Deloitte & Touche', 2, '6 Arch St #9757', 'Customizable neutral traveling'),
+('Congress Title', 13, '58 Hancock St', 'Customer loyalty'),
+('Kitchen People',	1, '3 E 31st St #77', 'Triple-buffered stable delivery'),
+('General Color Co Inc', 21, '6185 Bohn St #72', 'Focus group'),
+('Beck Corporation', 23, '21 E 64th Ave', 'Quality-focused 4th generation hardware')
 
-INSERT INTO Parts (SerialNumber, [Description], Price, VendorId) VALUES
-('WP8182119', 'Door Boot Seal',	117.86,	2),
-('W10780048', 'Suspension Rod', '42.81', 1),
-('W10841140', 'Silicone Adhesive', 6.77, 4),
-('WPY055980', 'High Temperature Adhesive', 13.94, 3)
+
+INSERT INTO Customers(FirstName, LastName, Age, Gender, PhoneNumber, CountryId) VALUES
+('Francoise', 'Rautenstrauch', 15, 'M', '0195698399', 5),
+('Kendra', 'Loud',	22, 'F', '0063631526', 11),
+('Lourdes', 'Bauswell', 50, 'M', '0139037043', 8),
+('Hannah', 'Edmison', 18, 'F', '0043343686', 1),
+('Tom', 'Loeza', 31, 'M', '0144876096', 23),
+('Queenie', 'Kramarczyk', 30, 'F', '0064215793', 29),
+('Hiu', 'Portaro', 25, 'M', '0068277755', 16),
+('Josefa', 'Opitz', 43, 'F', '0197887645', 17)
+
+GO;
+
+--03. Update
+
+UPDATE Ingredients
+SET DistributorId = 35
+WHERE Name = 'Bay Leaf' OR Name = 'Paprika' OR Name = 'Poppy'
+
+UPDATE Ingredients
+SET OriginCountryId = 14
+WHERE OriginCountryId = 8
+
+GO;
