efa041c7c0b5d623579736211fe726506a7359a2
RuslanKutsyy <44203840+RuslanKutsyy@users.noreply.github.com>
2020-08-19 10:33:46 +0300

    First Exam task 100/100, third task 84/100

---
+++ C# Course/C# Advanced/Exams/C# Advanced Retake Exam - 19 August 2020/Exam/FlowerWreaths/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlowerWreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lilies = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)));
+            var roses = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)));
+            var sumList = new List<int>();
+            int wreathsCount = 0;
+            int neededSum = 15;
+
+            while (true)
+            {
+                if (roses.Count == 0 || lilies.Count == 0)
+                {
+                    break;
+                }
+
+                var roseElement = roses.Peek();
+                var lilyElement = lilies.Peek();
+                var flowersSum = roseElement + lilyElement;
+
+                if (flowersSum > neededSum)
+                {
+                    var stackAsList = lilies.ToList();
+                    stackAsList[0] -= 2;
+                    stackAsList.Reverse();
+
+                    lilies = new Stack<int>(stackAsList);
+                }
+                else if (flowersSum == neededSum)
+                {
+                    wreathsCount += 1;
+                    lilies.Pop();
+                    roses.Dequeue();
+                }
+                else
+                {
+                    sumList.Add(flowersSum);
+                    lilies.Pop();
+                    roses.Dequeue();
+                }
+            }
+
+            if (sumList.Count > 0)
+            {
+                int additionalWreathes = sumList.Sum() / neededSum;
+                wreathsCount += additionalWreathes;
+            }
+
+            if (wreathsCount >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreathsCount} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - wreathsCount} wreaths more!");
+            }
+        }
+    }
+}
---
+++ C# Course/C# Advanced/Exams/C# Advanced Retake Exam - 19 August 2020/Exam/VetClinic/Clinic.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> data { get; set; }
+        public int Capacity { get; set; }
+
+        public Clinic(int capacity)
+        {
+            this.Capacity = capacity;
+            this.data = new List<Pet>();
+        }
+
+        public void Add(Pet pet)
+        {
+            if (this.data.Count < this.Capacity)
+            {
+                this.data.Add(pet);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            var pet = this.data.FirstOrDefault(x => x.Name == name);
+
+            if (pet == null)
+            {
+                return false;
+            }
+
+            this.data.Remove(pet);
+            return true;
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            var pet = this.data.FirstOrDefault(p => p.Name == name && p.Owner == owner);
+
+            return pet;
+        }
+
+        public Pet GetOldestPet()
+        {
+            var pet = this.data.OrderByDescending(p => p.Age).FirstOrDefault();
+            return pet;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.data.Count;
+            }
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("The clinic has the following patients:");
+            foreach (var pet in this.data)
+            {
+                sb.AppendLine(pet.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
