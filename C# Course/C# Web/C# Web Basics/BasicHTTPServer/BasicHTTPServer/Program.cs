3defe74f95c8f547f8ed210b0e61e7ecc2754f87
ruslan.kutsiy <ruslan.kutsiy@gmail.com>
2020-09-24 01:18:21 +0300

    Add first HTTPServer project and test ssh connection

---
+++ C# Course/C# Web/C# Web Basics/BasicHTTPServer/BasicHTTPServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicHTTPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
