f781b84d3beee3dadbcb1282f683937d735a9b67
ruslan.kutsiy <ruslan.kutsiy@gmail.com>
2021-01-17 18:47:10 +0200

    add HackerRank sockMerchant

---
+++ HakerRank Problems/HackerRank_Problems/HackerRank_Problems/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace HackerRank_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sockMerchant(9, "10 20 20 10 10 30 50 10 20".Split(" ").Select(x => int.Parse(x)).ToArray()));
+        }
+
+
+        static int sockMerchant(int n, int[] ar)
+        {
+            int pairs = 0;
+
+            var colors = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!colors.Contains(ar[i]))
+                {
+                    colors.Add(ar[i]);
+                }
+                else
+                {
+                    pairs++;
+                    colors.Remove(ar[i]);
+                }
+            }
+
+            //pairs /= 2;
+
+            return pairs;
+        }
+    }
+}
